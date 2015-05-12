9f2e61761c650cf9e0307b547426e4a2fcc59d26
Gerula <iacobandrei@gmail.com>
2015-05-11 17:52:04 -0700

    Lists problem 2

--- CrackingTheCodingInterview/2/2.rb
+++ CrackingTheCodingInterview/2/2.rb
@@ -1,4 +1,34 @@
 require_relative "linked_list"
 
-ll = Linked_list.new()
-puts ll.inspect
+list = Linked_list.new(0)
+list.fill_with_shit
+list.print_list
+
+class Linked_list
+    def contains?(element)
+       iterator = self
+       while iterator != element
+           if iterator.val == element.val
+               return true
+           end
+           iterator = iterator.link
+       end
+
+       return false
+    end
+
+    def duplicates_be_gone
+        iterator = self
+        while iterator && iterator.link
+            if contains?(iterator.link) 
+                iterator.link = iterator.link.link
+                puts "Deleted element #{iterator.val}"
+            end
+
+            iterator = iterator.link
+        end
+    end
+end
+
+list.duplicates_be_gone
+list.print_list
--- CrackingTheCodingInterview/2/linked_list.rb
+++ CrackingTheCodingInterview/2/linked_list.rb
@@ -4,6 +4,24 @@ class Linked_list
         @link = link
     end
 
+    def fill_with_shit
+        size = Random.rand(15)
+        head = self
+        1.upto(size).each {|i|
+            head.link = Linked_list.new(Random.rand(15))
+            head = head.link
+        }
+    end
+
+    def print_list
+        head = self
+        while head
+            print "#{head.val} "
+            head = head.link
+        end
+        puts
+    end
+
     attr_accessor :val
     attr_accessor :link
 end
