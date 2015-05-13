5822c3dc836d041023b74ee2a046e4137cb60ca4
Gerula <iacobandrei@gmail.com>
2015-05-12 20:30:12 -0700

    Stacks Problem 3 with actual stacks

--- CrackingTheCodingInterview/3/3.rb
+++ CrackingTheCodingInterview/3/3.rb
@@ -1,11 +1,31 @@
 # Imagine a (literal) stack of plates. If the stack gets too high, it might topple. There- fore, in real life, we would likely start a new stack when the previous stack exceeds some threshold. Implement a data structure SetOfStacks that mimics this. SetOf- Stacks should be composed of several stacks, and should create a new stack once the previous one exceeds capacity. SetOfStacks.push() and SetOfStacks.pop() should behave identically to a single stack (that is, pop() should return the same values as it would if there were just a single stack).
 #
 
+class Stack
+    def initialize(size)
+        @array = []
+        @max_size = size
+    end
+
+    def push(element)
+        unless @array.size == @max_size
+            @array.push(element)
+            return element
+        end
+        
+        return nil
+    end
+
+    def pop
+        @array.pop
+    end
+end
+
 class Stacks
     def initialize(number, size)
-        @arrays = []
+        @stacks = []
         number.times {
-            @arrays.push(Array.new(size))
+            @stacks.push(Stack.new(size))
         }
 
         @size = size
@@ -14,24 +34,25 @@ class Stacks
     end
 
     def pop
-        unless @current == 0
-            value = @array[@current / @size][@current % @size]
-            @current -= 1
-            return value
+        index = @max_elements - 1
+        value = nil
+        while !value && index > 0
+            value = @stacks[index].pop
+            index -= 1
         end
 
-        puts "Stack is empty"
-        return nil
+        return value
     end
 
     def push(element)
-        unless @current == @size * @max_elements - 1
-            @arrays[@current / @size][@current % @size] = element
-            @current += 1
-            return
+        index = 0
+        while !@stacks[index].push(element)
+            index += 1
         end
+    end
 
-        puts "Stack is full"
+    def inspect
+        @stacks.each { |x| puts x.inspect }
     end
 end
 
@@ -42,4 +63,11 @@ stacks.push(2)
 stacks.push(1)
 stacks.push(1)
 stacks.push(1)
+puts stacks.inspect
+stacks.pop
+stacks.pop
+puts stacks.inspect
+stacks.push("shit")
+stacks.push("futurama")
+stacks.push(Stacks.new(0, 0))
 puts stacks.inspect
