e321e3a4456966ee4fbe3384c8ee9adb6e9ee6d6
Gerula <iacobandrei@gmail.com>
2015-05-13 15:14:50 -0700

    Problem 4. yes yes its the same file but I was too lazy to do another file and build the tree - haven't slept at all last night.

--- CrackingTheCodingInterview/4/3.rb
+++ CrackingTheCodingInterview/4/3.rb
@@ -4,11 +4,11 @@
 class Node < Struct.new(:value, :left, :right)
 end
 
-def inorder(root)
-    unless root == nil
-        inorder(root.left)
-        puts root.value
-        inorder(root.right)
+def inorder(current)
+    unless current == nil
+        inorder(current.left)
+        puts current.value
+        inorder(current.right)
     end
 end
 
@@ -28,10 +28,56 @@ def buildTree(array, left, right)
     end
 end
 
-root = Node.new(1, nil, nil)
-root.left = Node.new(2, nil, nil)
-root.right = Node.new(3, nil, nil)
-inorder(root)
+current = Node.new(1, nil, nil)
+current.left = Node.new(2, nil, nil)
+current.right = Node.new(3, nil, nil)
+inorder(current)
 puts "----- separator -----"
-root = buildTree([1, 2, 3, 4, 5, 6, 7, 8, 9], 0, 9)
-inorder(root)
+current = buildTree([1, 2, 3, 4, 5, 6, 7, 8, 9], 0, 9)
+inorder(current)
+
+#  Given a binary search tree, design an algorithm which creates a linked list of all the nodes at each depth (i.e., if you have a tree with depth D, you’ll have D linked lists).
+
+class ListNode < Struct.new(:value, :next)
+end
+
+levels = {}
+level = 0 
+queue = [current]
+nodes_in_level = 1
+nodes_in_next_level = 0
+
+while queue.any?
+    current = queue.pop
+    new_node = ListNode.new(current, levels[level])
+    levels[level] = new_node 
+    nodes_in_level -= 1
+
+    if (current.left)
+        queue.unshift(current.left)
+        nodes_in_next_level += 1
+    end
+
+    if (current.right)
+        queue.unshift(current.right)
+        nodes_in_next_level += 1
+    end
+
+    if nodes_in_level == 0
+        level += 1
+        nodes_in_level = nodes_in_next_level
+        nodes_in_next_level = 0
+    end
+end
+
+levels.each do |level, list|
+    puts "Level #{level}"
+    idx = list
+    while idx
+        print " #{idx.value.value}"
+        idx = idx.next
+    end
+
+    puts
+end
+
