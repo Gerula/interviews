c5446a53017d4360e8677882f57b91d554092b8a
Gerula <iacobandrei@gmail.com>
2015-05-13 17:35:02 -0700

    more efficient

--- VariousArtists/perm_palind.rb
+++ VariousArtists/perm_palind.rb
@@ -1,22 +1,19 @@
 #  Write an efficient function that checks whether any permutation ↴ of an input string is a palindrome ↴ . 
 
 def is_perm_palind?(string)
-    frequencies = {}
+    uniques = []
     array = string.split("")
 
     array.each {|x|
-        frequencies[x] ||= 0
-        frequencies[x] += 1
+        if !uniques.include?(x)
+            uniques.push(x)
+        end
     }
 
-    if array.length % 2 == 0
-        return frequencies.values.all? { |x| x % 2 == 0}
-    else
-        return frequencies.values.count { |x| x % 2 == 1} == 1
-    end
+    uniques.length <= array.length/2 + 1 
 end
 
-["12312", "11223", "1122", "11542"].each { |x|
+["12312", "11223", "1122334", "1122", "11542", "123"].each { |x|
     puts "#{x} - #{is_perm_palind?(x)}"
 }
 
