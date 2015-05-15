b46775f36bcbc2d624ef01f55ff56eb5c8896272
Gerula <iacobandrei@gmail.com>
2015-05-14 20:07:37 -0700

    Make it fastergit add *

--- VariousArtists/nth_node.rb
+++ VariousArtists/nth_node.rb
@@ -2,14 +2,29 @@
 # It goes like this. Make a function to return the n_th node from a binary search tree.
 #
 
-class Node < Struct.new(:value, :left, :right)
+class Node < Struct.new(:value, :left, :right, :countleft, :countright)
 end
 
 def inorder root
     unless root.nil?
         inorder root.left
-        puts root.value
+        puts "val #{root.value} c #{root.countleft} #{root.countright}"
         inorder root.right
+    end
+end
+
+def find_n_th root, n
+    while root
+        if root.countleft + 1 == n
+            return root
+        end
+
+        if root.countleft < n
+            root = root.right
+            n = n - root.countleft + 1
+        else
+            root = root.left
+        end
     end
 end
 
@@ -19,11 +34,21 @@ def fill array, l, r
     end
 
     middle = l + (r - l) / 2
-    newNode = Node.new array[middle], nil, nil
+    newNode = Node.new array[middle], nil, nil, 0, 0
     
     unless l == r
         newNode.left = fill array, l, middle - 1
         newNode.right = fill array, middle + 1, r
+        
+        if newNode.left
+            newNode.countleft += 1
+            newNode.countleft += newNode.left.countleft + newNode.left.countright
+        end
+
+        if newNode.right
+            newNode.countright += 1
+            newNode.countright += newNode.right.countleft + newNode.right.countright
+        end
     end
 
     return newNode
@@ -52,4 +77,4 @@ end
 
 tree = fill [1, 2, 3, 4, 5, 6, 7, 8], 0, 8
 #inorder tree
-puts n_th(tree, 2).value
+puts find_n_th(tree, 2).value
