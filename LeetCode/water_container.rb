85f1a3dfe2e2e283ae02c412f3dd1015b88a5719
Gerula <iacobandrei@gmail.com>
2015-05-18 20:56:28 -0700

    max water container take 2

--- LeetCode/water_container.rb
+++ LeetCode/water_container.rb
@@ -11,7 +11,7 @@ def max_water_1(a)
     max_area = 0
 
     while i < j
-        area = (j - i) * [a[i], a[j]].min
+        area = (j - i - 1) * [a[i], a[j]].min
         if area > max_area
             max_area = area
             max_i = i
@@ -28,4 +28,27 @@ def max_water_1(a)
     puts "#{max_i} - #{max_j} = #{max_area}"
 end
 
+def max_water_2(a)
+    max = 0
+    left_max, right_max = [], []
+    0.upto(a.length - 1).each {|i|
+        left_max[i] = max
+        max = a[i] if (a[i] > max)
+    }
+    
+    max = 0
+    (a.length-1).downto(0).each {|i|
+        right_max[i] = max
+        max = a[i] if (a[i] > max)
+    }
+
+    sum = 0
+    (0..a.length - 1).each {|i|
+        sum += [[left_max[i], right_max[i]].min - a[i], 0].max
+    }
+
+    puts sum
+end
+
 max_water_1(a)
+max_water_2(a)
