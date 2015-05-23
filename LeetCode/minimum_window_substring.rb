a803412d224a21c56bd50afbfd2d224eeb412861
Gerula <iacobandrei@gmail.com>
2015-05-22 21:52:01 -0700

    Min window substring. I am a tired idiot

--- LeetCode/minimum_window_substring.rb
+++ LeetCode/minimum_window_substring.rb
@@ -1,4 +1,4 @@
-#  Given a string S and a string T, find the minimum window in S which will contain all the characters in T in complexity O(n).
+# Given a string S and a string T, find the minimum window in S which will contain all the characters in T in complexity O(n).
 #
 #  For example,
 #  S = "ADOBECODEBANC"
@@ -6,52 +6,46 @@
 #
 #  Minimum window is "BANC". 
 #
-#  I have an idea, I didn't test it out with peepee (pen+paper) I just want to blurt it out. Pro-tip: during tech interviews, I always look for the blurt out candidate. He will make best employee of the month
-#
+
+require 'set'
 
 def min_window(s, subs)
     size = s.size
-    left = Array.new(size)
-    right = Array.new(size)
-    
-    targets = subs.split("").inject({}) { |acc, x|
-        acc.merge({x => true})
+    right = 0
+    min_left = 0
+    min_right = s.size
+    target_size = subs.size
+
+    targets = Set.new
+    subs.split("").each {|x|
+        targets << x
     }
 
-    found_so_far = []
+    found_count = {}
 
-    0.upto(size - 1).each {|i|
-        if targets.has_key?(s[i])
-            found_so_far << s[i] unless found_so_far.include?(s[i])
-        end
-        
-        left[i] = found_so_far.length
-    }
-  
-    puts left.inspect
-    found_so_far.clear 
-    (size - 1).downto(0).each {|i|
-        if targets.has_key?(s[i])
-            found_so_far << s[i] unless found_so_far.include?(s[i])
-        end
-        
-        right[i] = found_so_far.length
-    }
-
-    puts right.inspect
-    both = left.zip(right).map {|x, y| x + y}
-
-    result = []
     0.upto(size - 1).each { |i|
-        if both[i] == subs.size
-            result << s[i]
+        right += 1
+        current = s[i]
+        if targets.include?(current)
+            found_count[current] ||= 0
+            found_count[current] += 1
+
+            if (found_count.size == targets.size)
+                while !targets.include?(s[min_left]) || found_count[s[min_left]] > 1
+                    found_count[s[min_left]] -= 1 unless found_count[s[min_left]].nil?
+                    min_left += 1
+                end
+
+                min_right = right
+            end
         end
     }
 
-    return result
+    return s[min_left..min_right]
 end
 
 [["ADOBECODEBANC","ABC"]].each{ |x|
+    puts x.inspect
     puts min_window(x.shift, x.first) # for extra readability - [0]-[1] is for peasants
 }
 
