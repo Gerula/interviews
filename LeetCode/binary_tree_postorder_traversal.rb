a9c2979b2ad4733e91a580857b3ca1f787c35e72
Gerula <iacobandrei@gmail.com>
2015-05-25 07:38:07 -0700

    Inorder traversal iterative

---
+++ LeetCode/binary_tree_postorder_traversal.rb
@@ -0,0 +1,45 @@
+# Binary tree traversals
+#
+
+class Node < Struct.new(:value, :left, :right)
+end
+
+class Tree
+    def initialize(size)
+        @root = fill(0, size - 1)
+    end
+
+    def fill(left, right)
+        unless left > right
+            mid = left + (right - left) / 2
+            return Node.new(mid,
+                            fill(left, mid - 1),
+                            fill(mid + 1, right))
+        end
+
+        return nil
+    end
+
+    def inorder
+        current = @root
+        stack = []
+        while !current.nil? || stack.any?
+            if current.nil?
+                current = stack.pop
+                yield current
+                current = current.right
+            else
+                stack.push(current)
+                current = current.left
+            end
+        end
+    end
+
+    private :fill
+end
+
+tree = Tree.new(15)
+puts "--- Inorder ---"
+tree.inorder {|x|
+    puts x.value
+}
