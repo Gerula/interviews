b38ea55f266d80f878bd72c03aef583402d25306
Gerula <iacobandrei@gmail.com>
2015-05-24 20:13:52 -0700

    BST recovery with inorder not thread safe ~^_^

--- LeetCode/recover_binary_search_tree.rb
+++ LeetCode/recover_binary_search_tree.rb
@@ -10,7 +10,10 @@ class Tree
     attr_reader :root
 
     def initialize
-        @root = Tree.generate(0, Random.rand(15))
+        @root = Tree.generate(1, Random.rand(15))
+        @first = nil
+        @second = nil
+        @prev = nil
     end
 
     def self.generate(left, right)
@@ -36,6 +39,11 @@ class Tree
         one.value, two.value = two.value, one.value
     end
 
+    def unswap_random
+        find_swap(@root)
+        @first.value, @second.value = @second.value, @first.value
+    end
+    
     def get_random_node
         it = @root
         Random.rand(1..5).times {
@@ -45,18 +53,44 @@ class Tree
         return it
     end
 
+    def to_s
+        inorder(@root)
+    end
+
+    def inorder(root)
+        unless root.nil?
+            inorder(root.left)
+            print "#{root.value} "
+            inorder(root.right)
+        end
+    end
+
+    def find_swap(root)
+        unless root.nil?
+            find_swap(root.left)
+            if !@prev.nil? && root.value < @prev.value
+                if @first.nil?
+                    @first = @prev
+                else
+                    @second = root
+                end
+            end
+            @prev = root
+            find_swap(root.right)
+        end
+    end
+
+    private :find_swap
+    private :inorder
     private :get_random_node
 end
 
-def inorder(root)
-    unless root.nil?
-        inorder(root.left)
-        puts(root.value)
-        inorder(root.right)
-    end
-end
+$first = nil
+$second = nil
 
 tree = Tree.new
-inorder(tree.root)
+puts tree.to_s
 tree.swap_random
-inorder(tree.root)
+puts tree.to_s
+tree.unswap_random
+puts tree.to_s
