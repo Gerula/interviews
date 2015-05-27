5fe08c8284bffe1f973d69238d563e5b603e635e
Gerula <iacobandrei@gmail.com>
2015-05-27 07:11:35 -0700

    Roman numerals with validation and two-way conversion

--- LeetCode/roman_to_integer.rb
+++ LeetCode/roman_to_integer.rb
@@ -21,6 +21,15 @@ class Fixnum
           1 => "I"
       }  
     end
+
+    def to_roman
+        num = self
+        Fixnum.roman_mapping.inject("") { |acc, digits|
+            digit, string = digits
+            div, num = num.divmod(digit)
+            acc << string * div 
+        }
+    end
 end
 
 class String
@@ -40,7 +49,61 @@ class String
 
         return value
     end
+
+    def to_i_roman_validate
+        s = self.dup
+        value = 0
+        while !s.empty?
+            pos = 1
+            case 
+            when s.start_with?("XXXX") then
+                raise("too many Xs")
+            when s.start_with?("IIII") then
+                raise("too many Is")
+            when s.start_with?("VV") then
+                raise("too many Vs")
+            when s.start_with?("CM") then
+                value += 900
+                pos = 2
+            when s.start_with?("CD") then
+                value += 400
+                pos = 2
+            when s.start_with?("XC") then
+                value += 90
+                pos = 2
+            when s.start_with?("XL") then
+                value += 40
+                pos = 2
+            when s.start_with?("IX") then
+                value += 9
+                pos = 2
+            when s.start_with?("IV") then
+                value += 4
+                pos = 2
+            when s.start_with?("M") then
+                value += 1000
+            when s.start_with?("D") then
+                value += 500
+            when s.start_with?("C") then
+                value += 100
+            when s.start_with?("L") then
+                value += 50
+            when s.start_with?("X") then
+                value += 10
+            when s.start_with?("V") then
+                value += 5
+            when s.start_with?("I") then 
+                value += 1
+            else 
+                raise("Invalid numeral")
+            end
+
+            s = s[pos..-1]
+        end
+
+        return value
+    end
 end
 
-puts ["I", "IV", "V",  "XCVIII", "MDCCC", "LXXXVIII","XXXIX"].map{|x| {x => x.to_i_roman}}.inspect
+puts ["I", "IV", "V",  "XCVIII", "MDCCC", "LXXXVIII","XXXIX"].map{|x| {x => [x.to_i_roman, x.to_i_roman_validate, x.to_i_roman.to_roman]}}.inspect
 
