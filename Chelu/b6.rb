fb3c3fc139bd00938e1dcc8faf029d08ca4697c2
Gerula <iacobandrei@gmail.com>
2015-05-30 10:07:20 -0700

    Quiz

---
+++ Chelu/b6.rb
@@ -0,0 +1,16 @@
+#  Describe abstraction, encapsulation, polymorphism. 
+#  
+#  To my eternal shame I will try to answer them directly and update them in another commit with the academic rigurous answers
+#
+#  Abstraction = The process in which an object hides implementation details about it's behavior.
+#
+#  Encapsulation = The packaging of data and behavior about a certain concept in a class
+#
+#  Polymorphism = Object having different behaviors in different contexts (multiple forms)
+#  
+#  is-a and has-a? = inheritance vs composition. Is-a relationship is when the subsitution property holds: for any class B inheriting A, P(B) => P(A). Has-a relationship describes the practice of having another object as a property. Both are related to code-reuse.
+#
+#  Abstract class vs interface? = Interface is a contract: does not contain any behaviors/code but only signatures, cannot be instatiated, you can inherit from multiple interfaces. Abstract class may contain code, must contain at least one virtual function, cannot be instantiated, in order to be used it needs to be inherited.
+#
+#  Class vs instance? = Class is a template for creating instances. Class is a type, instance is an object belonging to a type.
+#
