48ed7fc8b61a507b76b1e59820fd7190c4cfa2dd
Gerula <iacobandrei@gmail.com>
2015-06-07 16:04:33 -0700

    Rearrange permutation ^_^

--- VariousArtists/shuffling.rb
+++ VariousArtists/shuffling.rb
@@ -1,11 +1,22 @@
 # shuffle deck of cards
 #
 
-array = Array(1..10)
+perm = Array(0..4)
 
-0.upto(array.size - 2).each { |i|
-    target = Random.rand((i + 1)..(array.size-1))
-    array[target], array[i] = array[i], array[target]
+0.upto(perm.size - 2).each { |i|
+    target = Random.rand((i + 1)..(perm.size-1))
+    perm[target], perm[i] = perm[i], perm[target]
 }
 
-puts array.inspect
+array = ["zero", "one", "two", "three", "four"]
+
+puts perm.inspect
+
+0.upto(perm.size - 1).each { |i|
+    while perm[i] != i && perm[i] < perm[perm[i]]
+        x = perm[i]
+        perm[i], perm[x] = perm[x], perm[i]
+    end
+}
+
+puts perm.inspect
