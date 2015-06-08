ba3dce6c5a86191403b2393a2772f8332517804a
Gerula <iacobandrei@gmail.com>
2015-06-08 07:23:43 -0700

    Set matrix zeroes using first row and first column

--- LeetCode/set_matrix_zeroes.rb
+++ LeetCode/set_matrix_zeroes.rb
@@ -15,27 +15,52 @@ matrix = [[0, 1, 1, 1, 0, 1],
           [1, 1, 1, 1, 1, 1],
           [1, 1, 1, 1, 1, 1]]
 
-0.upto(matrix.size-1).each { |i|
-    0.upto(matrix[0].size-1).each { |j|
-        if matrix[i][j] == 0
-            0.upto(matrix.size-1).each { |k|
-                matrix[i][k] = nil unless matrix[i][k] == 0
-                matrix[k][j] = nil unless matrix[k][j] == 0
-            }
-        end
-    }
-}
 
 matrix.each { |x|
     puts x.inspect
 }
 puts
 
-matrix.each { |x|
-    x.map!{ |x|
-        x.nil? ? 0 : x
+first_column_zero = false
+first_row_zero = false
+
+0.upto(matrix.size - 1).each { |i|
+    if matrix[0][i] == 0
+        first_row_zero = true
+    end
+    if matrix[i][0] == 0
+        first_column_zero = true
+    end
+}
+
+0.upto(matrix.size - 1).each { |i|
+    0.upto(matrix.size - 1).each { |j|
+        if matrix[i][j] == 0
+            matrix[i][0] = 0
+            matrix[0][j] = 0
+        end
     }
 }
+
+1.upto(matrix.size - 1).each { |i|
+    1.upto(matrix.size - 1).each { |j|
+        if matrix[0][j] == 0 || matrix[i][0] == 0
+            matrix[i][j] = 0
+        end
+    }
+}
+
+if first_row_zero
+    0.upto(matrix.size - 1).each { |i|
+        matrix[0][i] = 0
+    }
+end
+
+if first_column_zero
+    0.upto(matrix.size - 1).each { |i|
+        matrix[i][0] = 0
+    }
+end
 
 matrix.each { |x|
     puts x.inspect
