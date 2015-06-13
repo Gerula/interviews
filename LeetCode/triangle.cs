ee3f9ac4a2eed92929e034d48567ac50648c16e2
Gerula <iacobandrei@gmail.com>
2015-06-13 12:12:14 -0700

    Triangle in C# science fiction

--- LeetCode/triangle.cs
+++ LeetCode/triangle.cs
@@ -18,13 +18,31 @@ using System.Collections.Generic;
 using System.Linq;
 
 class Program {
+
+    private static int Ninja(List<List<int>> triangle) {
+       return Enumerable.Range(1, triangle.Count - 1)
+                        .Aggregate(triangle[0],(a, b) => {
+                                   List<int> result = new List<int>(triangle[b]); 
+                                   for (int i = 0; i < result.Count; i++)
+                                   {
+                                        int first = i == 0 ? int.MaxValue : a[i - 1]; 
+                                        int second = i == result.Count - 1 ? int.MaxValue : a[i];
+                                        result[i] += Math.Min(first, second);
+                                   }
+
+                                   return result;
+                                } ).Min();
+    }
+
     public static void Main(string[] args) {
         List<List<int>> triangle = new List<List<int>>();
         triangle.Add(new List<int> { 2 } );
         triangle.Add(new List<int> { 3, 4 } );
         triangle.Add(new List<int> { 6, 5, 7 } );
         triangle.Add(new List<int> { 4, 1, 8, 3 } );
-        
+    
+        Console.WriteLine(Ninja(triangle));    
+
         for (int i = 1; i < triangle.Count; i++)
         {
             for (int j = 0; j < triangle[i].Count; j++)
