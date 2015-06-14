65185971ed21ae2597df71fd4a945aa0a5d74004
Gerula <iacobandrei@gmail.com>
2015-06-13 21:47:14 -0700

    Is tree balanced? C#

--- LeetCode/validate_bst.cs
+++ LeetCode/validate_bst.cs
@@ -5,8 +5,8 @@ using System.Text;
 
 class Node {
     public int Value { get; set; }
-    public Node Left { get; private set; }
-    public Node Right { get; private set; }
+    public Node Left { get; set; }
+    public Node Right { get; set; }
 
     public Node(int value, Node left, Node right) {
         Value = value; Left = left; Right = right;
@@ -32,7 +32,8 @@ class Tree {
     public override string ToString() {
         StringBuilder result = new StringBuilder();
         Inorder(_root, result);
-        result.Append(String.Format("IsBst? {0}", IsBst()));
+        result.Append(String.Format(" IsBst? {0}", IsBst()));
+        result.Append(String.Format(" IsBalanced? {0}", IsBalanced()));
         return result.ToString();
     }
 
@@ -46,6 +47,18 @@ class Tree {
 
     public void ScrewWithIt() {
         _root.Left.Right.Value = int.MaxValue;
+        Node it = _root;
+        while (it.Left != null) {
+            it = it.Left;
+        }
+
+        it.Left = new Node(10,
+                           new Node(11,
+                                    new Node(12,
+                                             new Node(13, null, null),
+                                             null),
+                                    null),
+                           null);
     }
 
     public bool IsBst() {
@@ -60,6 +73,24 @@ class Tree {
         return (min <= current.Value && current.Value <= max) &&
                 IsBst(current.Left, min, current.Value) &&
                 IsBst(current.Right, current.Value, max);
+    }
+
+    public bool IsBalanced() {
+        return IsBalanced(_root) != -1;
+    }
+
+    private int IsBalanced(Node current) {
+        if (current == null) {
+            return 0;
+        }
+
+        int left = IsBalanced(current.Left);
+        int right = IsBalanced(current.Right);
+        if ((Math.Abs(left - right) > 1) || (left == -1) || (right == -1)) {
+            return -1;
+        } else {
+            return Math.Max(left, right) + 1;
+        }
     }
 }
 
