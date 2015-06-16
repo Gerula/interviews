935cccbf6f8aab2c3b19e21ed0d702d14bef23c3
Gerula <iacobandrei@gmail.com>
2015-06-16 07:04:36 -0700

    Swap bits

--- ElementsOfProgrammingInterviews/PrimitiveTypes/reverse_bits.cs
+++ ElementsOfProgrammingInterviews/PrimitiveTypes/reverse_bits.cs
@@ -1,9 +1,19 @@
 using System;
 
 class Program {
-    
+    static byte reverse(byte x) {
+        int y = 0;
+        while (x != 0) {
+            y = (y << 1) | (x & 1);
+            x >>= 1;
+        }
+
+        return (byte) y;
+    }    
 
     public static void Main(string[] args) {
-    
+        byte x = (byte) new Random().Next(10, 255);
+        byte y = reverse(x);
+        Console.WriteLine("{0} - {1} - {2} - {3}", x, Convert.ToString(x, 2), y, Convert.ToString(y, 2));
     }
 }
