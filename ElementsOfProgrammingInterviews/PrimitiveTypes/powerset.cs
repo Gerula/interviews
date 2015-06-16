a59a6d01fcd9a4a7aee5377093825be9b0f9749c
Gerula <iacobandrei@gmail.com>
2015-06-16 07:15:43 -0700

    Powerset with recursion. It's like a poem that I need to learn. Pisses me off..

--- ElementsOfProgrammingInterviews/PrimitiveTypes/powerset.cs
+++ ElementsOfProgrammingInterviews/PrimitiveTypes/powerset.cs
@@ -4,6 +4,17 @@ using System.Collections.Generic;
 using System.Linq;
 
 class Program {
+
+    private static void PowerSet(List<String> list, int count, List<String> result) {
+        Console.WriteLine(String.Join(",", result));
+
+        for (int i = count; i < list.Count(); i++) {
+            result.Add(list[i]);
+            PowerSet(list, i + 1, result);
+            result.RemoveAt(result.Count() - 1);
+        }
+    }
+
     public static void Main(string[] args) {
         List<String> set = new List<String> {"Red", "White", "Blue"};
         List<String> result = new List<String>();
@@ -18,5 +29,8 @@ class Program {
 
             Console.WriteLine(String.Join(",", result));
         }
+
+        Console.WriteLine();
+        PowerSet(set, 0, new List<String>());
     }
 }
