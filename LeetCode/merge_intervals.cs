f534a33d9d10cc9a1536c2ad65ec3aa43ad8d53d
Gerula <iacobandrei@gmail.com>
2015-06-18 19:38:32 -0700

    Merge intervals. O(n) as opposed to O(nlogn) but with O(k) space as opposed to O(1)

--- LeetCode/merge_intervals.cs
+++ LeetCode/merge_intervals.cs
@@ -3,6 +3,36 @@ using System.Collections.Generic;
 using System.Linq;
 
 class Program {
+
+    static List<Tuple<int, int>> merge(List<Tuple<int, int>> intervals) {
+        int[] serialized = Enumerable.Repeat(0, 100).ToArray();
+        intervals.ForEach(x => { 
+                    for (int i = x.Item1; i<= x.Item2; i++) {
+                        serialized[i] = 1;
+                    }
+                });
+
+        intervals.Clear();
+        bool found = false;
+        int left = 0;
+        int right = 0;
+        for (int i = 0; i < 100; i++) {
+            if ((serialized[i] == 1) && !found) {
+                found = true;
+                left = i;
+                continue;
+            }
+
+            if (found && (serialized[i] == 0 || i == 100 - 1)) {
+                found = false;
+                right = i - 1;
+                intervals.Add(new Tuple<int, int>(left, right));
+            }
+        }
+
+        return intervals;
+    }
+
     static void Main() {
         List<Tuple<int, int>> intervals = new List<Tuple<int, int>> {
             new Tuple<int, int> (2, 6),
@@ -12,6 +42,9 @@ class Program {
         };
 
         Console.WriteLine(String.Join(",", intervals.Select(x => String.Format("[{0}, {1}]", x.Item1, x.Item2))));
+        List<Tuple<int, int>> intervals2 = new List<Tuple<int, int>>();
+        intervals2.AddRange(intervals);
+        Console.WriteLine("Second method : {0}", String.Join(",", merge(intervals2).Select(x => String.Format("[{0}, {1}]", x.Item1, x.Item2))));
         intervals = intervals.OrderBy(x => x.Item1).ToList();
 
         int i = 0;
