543118a63a7f31143b77a4c2caba1164dc4a39db
Gerula <iacobandrei@gmail.com>
2015-06-19 20:28:24 -0700

    Two sum in C# with Linq but it doesn't work with duplicates due to the ToDictionary method. The 9th grade solution works. Go figure.. I need to graduate 9th grade #im14andthisisfunny

--- LeetCode/two_sum.cs
+++ LeetCode/two_sum.cs
@@ -1,7 +1,14 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 class Program {
+
+    static Tuple<int, int> TwoSum2(int[] input, int sum) {
+        var hash = input.ToList().ToDictionary(x => x, x => Array.IndexOf(input, x));
+        return hash.Where(x => hash.ContainsKey(sum - x.Key)).Select(x => new Tuple<int, int>(x.Value + 1, hash[sum - x.Key] + 1)).First();
+    }
+
     static Tuple<int, int> TwoSum(int[] input, int sum) {
         Dictionary<int, int> indexes = new Dictionary<int, int>();
         for (int i = 0; i < input.Length; i++) {
@@ -16,7 +23,8 @@ class Program {
     }
 
     static void Main() {
-        int[] input = new int[] {2, 2, 7, 11, 15};
-        Console.WriteLine(TwoSum(input, 4));
+        int[] input = new int[] {2, 7, 11, 15};
+        Console.WriteLine(TwoSum(input, 9));
+        Console.WriteLine(TwoSum2(input, 9));
     }
 }
