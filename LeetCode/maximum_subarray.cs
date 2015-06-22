ccde4d522d9fb3cf10c3ba364ffa02e31e2abed9
Gerula <iacobandrei@gmail.com>
2015-06-21 20:16:40 -0700

    Max subarray sum. Completely fucking useless solution using divide and conquer

--- LeetCode/maximum_subarray.cs
+++ LeetCode/maximum_subarray.cs
@@ -2,6 +2,38 @@ using System;
 using System.Collections.Generic;
 
 class Program {
+    static int Sum(List<int> a, int left, int mid, int right) {
+        int max_left = int.MinValue;
+        int max_right = int.MinValue;
+        int sum = 0;
+        for (int i = left; i <= mid; i++) {
+            sum += a[i];
+            max_left = Math.Max(max_left, sum);
+        }
+
+        sum = 0;
+        for (int i = mid + 1; i <= right; i++) {
+            sum += a[i];
+            max_right = Math.Max(max_right, sum);
+        }
+
+        return max_left + max_right;
+    }
+    
+    static int MaxSum(List<int> a, int left, int right) {
+        if (left == right) {
+            return a[left];
+        }
+
+        int mid = left + (right - left) / 2;
+        return Math.Max(
+                        Math.Max(
+                                 MaxSum(a, left, mid), 
+                                 MaxSum(a, mid + 1, right)), 
+                        
+                        Sum(a, left, mid, right));
+    }
+
     static void Main() {
         List<int> a = new List<int>();
         a.Add(-2); a.Add(1); a.Add(-3); a.Add(4); a.Add(-1); a.Add(2); a.Add(1); a.Add(-5); a.Add(4);
@@ -14,5 +46,6 @@ class Program {
         }
 
         Console.WriteLine(global_max);
+        Console.WriteLine(MaxSum(a, 0, a.Count - 1));
     }
 }
