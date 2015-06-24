081ba17359dbe66222bdd5caa14de968afc1859c
Gerula <iacobandrei@gmail.com>
2015-06-23 18:36:09 -0700

    Word break DP

--- ElementsOfProgrammingInterviews/BinaryTrees/word_break.rb
+++ ElementsOfProgrammingInterviews/BinaryTrees/word_break.rb
@@ -16,6 +16,36 @@ def word_break_naive(phrase, dictionary)
     end
 end
 
+def word_break(phrase, dictionary)
+    wb = Array.new(phrase.size + 1).map { false }
+    
+    0.upto(phrase.size).each { |i| 
+        if !wb[i] && dictionary.include?(phrase[0..i - 1])
+            puts phrase[0..i - 1]
+            wb[i] = true
+        end
+
+        if wb[i]
+            if i == phrase.size
+                return true
+            end
+            
+            (i + 1).upto(phrase.size).each { |j|
+                if !wb[j] && dictionary.include?(phrase[i..j - 1])
+                    puts phrase[i..j - 1]
+                    wb[j] = true
+                end
+
+                if j == phrase.size && wb[j]
+                    return true
+                end
+            }
+        end
+    }
+
+    return false
+end
+
 phrases.each { |x|
-    puts "#{x} - #{word_break_naive(x, dictionary)}"
+    puts "#{x} - #{word_break_naive(x, dictionary)} - #{word_break(x, dictionary)}"
 }
