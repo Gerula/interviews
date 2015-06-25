495673a546ca365a40e1bb05566a56e79dc57d4c
Gerula <iacobandrei@gmail.com>
2015-06-25 07:06:06 -0700

    BST to single linked list flattened

--- ElementsOfProgrammingInterviews/BinaryTrees/bst_to_double_linked_list.cs
+++ ElementsOfProgrammingInterviews/BinaryTrees/bst_to_double_linked_list.cs
@@ -36,8 +36,42 @@ class Program {
         };
     }
 
+    static Node ToSingleList(Node root) {
+        if (root == null) {
+            return null;
+        }
+
+        Node left = root.Left;
+        Node right = root.Right;
+        root.Left = null;
+        root.Right = null;
+
+        left = ToSingleList(left);
+        right = ToSingleList(right);
+
+        Node last = root;
+        if (left != null) {
+            root.Right = left;
+            while (last.Right != null) {
+                last = last.Right;
+            }
+        }
+        
+        last.Right = right;
+
+        return root;
+    }
+
     static void Main() {
         Node root = Fill(1, 10);
         Console.WriteLine(String.Join(",", Inorder(root).Select(x => x.Value)));
+        root = ToSingleList(root);
+        Node it = root;
+        while (it != null) {
+            Console.Write("{0} ", it.Value);
+            it = it.Right;
+        }
+
+        Console.WriteLine();
     }
 }
