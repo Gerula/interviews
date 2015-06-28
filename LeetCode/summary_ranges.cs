27fa464d7626950e25bf99ab4c58cb7961ea7352
Gerula <iacobandrei@gmail.com>
2015-06-28 13:21:26 -0700

    Summary ranges - highschool style

--- LeetCode/summary_ranges.cs
+++ LeetCode/summary_ranges.cs
@@ -5,18 +5,23 @@ using System.Linq;
 class Program {
     static void Main() {
         int[] a = new int[] {0, 1, 2, 4, 5, 7};
-        int[] set = Enumerable.Repeat(-1, a.Max() + 1).ToArray(); 
-        Console.WriteLine(String.Join(",",
-                          a.Aggregate(new Dictionary<int, int>(), (agg, b) => {
-                               if (b >= 1 && set[b - 1] != -1) {
-                                   agg[set[b - 1]] = b;
-                                   set[b] = set[b - 1];                
-                               } else {
-                                   agg[b] = b;
-                                   set[b] = b;
-                               }
+        int start = 0;
+        int end = 0;
+        for (int i = 1; i < a.Length + 1; i++) {
+            if (i < a.Length && a[i] - a[i - 1] == 1) { 
+                end++;
+            } else {
+                if (end == start) {
+                    Console.Write("[{0}]", a[start]);
+                } else {
+                    Console.Write("[{0} => {1}]", a[start], a[end]);
+                }
 
-                               return agg;
-                          }).Select(x => String.Format("[{0} => {1}]", x.Key, x.Value))));
+                start = i;
+                end = i;
+            }
+        }
+
+        Console.WriteLine();
     }
 }
