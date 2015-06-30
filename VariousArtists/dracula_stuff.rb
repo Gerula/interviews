545e8e4e0906707df1b9be633e25f2d2fe89483f
Gerula <iacobandrei@gmail.com>
2015-06-29 19:02:13 -0700

    Counting sort - simplr

--- VariousArtists/dracula_stuff.rb
+++ VariousArtists/dracula_stuff.rb
@@ -4,26 +4,18 @@
 a = 0.upto(15).map {|x| Random.rand(0..20) }
 puts a.inspect
 
-k = 5
-c = 0
 counts = Array.new(a.max + 1, 0)
 a.each { |x|
-    if c == k 
-        puts "#{k}th element is #{x}"
-    end
-    c += 1
     counts[x] += 1
 }
 
-1.upto(counts.size - 1).each { |i|
-    counts[i] += counts[i - 1]   
-}
-
-t = []
-a.each { |x|
-    t[counts[x] - 1] = x;
-    counts[x] -= 1
-}
+t = counts.each_with_index.map { |x, i|
+    if x != 0
+        Array.new(x, i)
+    else
+        nil
+    end
+}.select { |x| x != nil }.flatten!
 
 puts t.inspect
 
