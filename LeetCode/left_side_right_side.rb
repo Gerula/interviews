90224300733e4269b10fb4eeaaecd5811dedc1c8
Gerula <iacobandrei@gmail.com>
2015-07-02 19:08:53 -0700

    Some tree stuff

---
+++ LeetCode/left_side_right_side.rb
@@ -0,0 +1,37 @@
+class Node < Struct.new(:value, :left, :right)
+    def self.fill(low, high)
+        return nil if low > high
+        mid = low + (high - low) / 2
+        return Node.new(mid, Node::fill(low, mid - 1), Node::fill(mid + 1, high))
+    end
+
+    def display
+        Node::inorder(self)
+        puts
+    end
+
+    def self.inorder(node)
+        unless node.nil?
+            inorder(node.left)
+            print "#{node.value} "
+            inorder(node.right)
+        end
+    end
+
+    def n_th(n)
+        input = [n, nil]
+        Node::nth(self, input)
+        return input[1]
+    end
+
+    def self.nth(root, input)
+        Node::nth(root.left, input) unless root.left.nil?
+        input[0] -= 1
+        input[1] = root if input[0] == 0
+        Node::nth(root.right, input) unless root.right.nil?
+    end
+end
+
+root = Node::fill(1, 10)
+root.display
+puts root.n_th(3)
