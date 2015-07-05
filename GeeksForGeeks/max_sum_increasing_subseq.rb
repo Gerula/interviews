3ab227649d340656312fe0da4b8fa2ff27ef0958
Gerula <iacobandrei@gmail.com>
2015-07-05 14:25:04 -0700

    Max sum increasing subseq with subseq

--- GeeksForGeeks/max_sum_increasing_subseq.rb
+++ GeeksForGeeks/max_sum_increasing_subseq.rb
@@ -1,14 +1,18 @@
 class Array
     def max_sum_sub
         subseq = self.dup
+        sub = subseq.map { |x| [x] }
         1.upto(self.size - 1).each do |i|
             0.upto(i - 1).each do |j|
                 if self[i] > self[j] && subseq[i] < subseq[j] + self[i]
                     subseq[i] = subseq[j] + self[i]
+                    sub[i] += sub[j]
+                    sub[i] = sub[i].uniq
                 end
             end
         end
-
+        
+        puts sub.detect{ |x| x.reduce(:+) == subseq.max }.inspect
         return subseq.max
     end
 end
