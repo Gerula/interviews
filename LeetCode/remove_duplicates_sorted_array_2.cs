3d6dcba826f4e151db2a570f782f18bc33c8eaa0
Gerula <iacobandrei@gmail.com>
2015-07-07 07:28:34 -0700

    Remove duplicates from sorted array II - Linq

--- LeetCode/remove_duplicates_sorted_array_2.cs
+++ LeetCode/remove_duplicates_sorted_array_2.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Linq;
+using System.Collections.Generic;
 
 static class Program {
     static int RemoveDupes(this int[] a) {
@@ -21,11 +23,28 @@ static class Program {
         return a.Length - count;
     }
 
+    static int[] RemoveDupes2(this int[] v) {
+        return v.Aggregate(new List<int>(),
+                           (a, b) => {
+                                if (a.Count(x => x == b) < 2) {
+                                    a.Add(b);
+                                }
+
+                                return a;
+                           }).ToArray();
+    }
+
     static void Main() {
         int[] a = new int[] {1, 1, 1, 2, 2, 3, 3, 3};
         int c = RemoveDupes(a);
         for (int i = 0; i < c; i++) {
             Console.Write("{0} ", a[i]);
+        }
+
+        Console.WriteLine();
+
+        foreach (int x in RemoveDupes2(a)) {
+            Console.Write("{0} ", x);
         }
 
         Console.WriteLine();
