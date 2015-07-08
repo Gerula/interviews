9e2351bd654f93897855954d0d8641753b882de6
Gerula <iacobandrei@gmail.com>
2015-07-07 19:06:33 -0700

    Powerset recursive

--- Labours/week02/subsets.rb
+++ Labours/week02/subsets.rb
@@ -14,7 +14,23 @@ class Array
 
         return result
     end
+
+    def power
+        results = []
+        powerbar([], results, 0)
+        return results
+    end
+
+    def powerbar(result, results, step)
+        results << result.dup
+        step.upto(self.size - 1).each { |i|
+            result.push(self[i])
+            powerbar(result, results, i + 1)
+            result.pop
+        }
+    end
 end
 
 puts [1, 2, 3].powerset.inspect
+puts [1, 2, 3].power.inspect
 
