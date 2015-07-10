ef116963055e5250d08e4756b8537c16464c6e0d
Gerula <iacobandrei@gmail.com>
2015-07-09 22:00:51 -0700

    Gray code 2

--- LeetCode/gray_code_3.rb
+++ LeetCode/gray_code_3.rb
@@ -1,14 +1,14 @@
 class Fixnum
     def gray_code
-        2.upto(self).inject([0, 1]) { |agg, x|
-            down = agg.map { |x| [0, x].flatten }
-            up = agg.reverse.map { |x| [1, x].flatten }
-            agg = down + up
-        }.map { |x|
-            x.reverse.map.with_index { |bit, power|
-                bit * 2 ** power
-            }.reduce(:+)
+        result = [0]
+        self.times {
+            size = result.size
+            (size - 1).downto(0).each { |j|
+                result << result[j] + size
+            }
         }
+
+        result
     end
 end
 
