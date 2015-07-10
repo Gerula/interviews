6b2a295998802b998ddd1afbe9c7eddb751b7a24
Gerula <iacobandrei@gmail.com>
2015-07-09 19:29:00 -0700

    Palindrome number

--- LeetCode/palindrome_number.cs
+++ LeetCode/palindrome_number.cs
@@ -3,10 +3,28 @@ using System.Linq;
 
 static class Program {
     static bool Palindrome(this int n) {
-        return n.ToString().ToCharArray().SequenceEqual(n.ToString().Reverse());
+        int m = n;
+        int div = 1;
+        while (m > 10) {
+            div *= 10;
+            m /= 10;
+        }
+
+        while (n != 0) {
+            int high = n / div;
+            int low = n % 10;
+            if (high != low) {
+                return false;
+            }
+
+            n = n % div / 10;
+            div /= 100;
+        }
+
+        return true;
     }
 
     static void Main() {
-        Console.WriteLine(String.Join(", ", Enumerable.Range(1000, 1000).Where(x => x.Palindrome())));
+        Console.WriteLine(String.Join(", ", Enumerable.Range(1000, 20000).Where(x => x.Palindrome())));
     }
 }
