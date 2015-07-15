72e1daaf5421dfceacc6fe25cb5aee10e37fb5bf
Gerula <iacobandrei@gmail.com>
2015-07-15 07:46:02 -0700

    Bitwise and of range

--- LeetCode/bitwise_and_of_range.rb
+++ LeetCode/bitwise_and_of_range.rb
@@ -7,4 +7,16 @@ def and_range(a, b)
     return mask & a
 end
 
+def and_range_2(a, b)
+    offset = 0
+    while a != b
+        offset += 1 
+        a >>= 1
+        b >>= 1
+    end
+
+    return a << offset
+end
+
 puts and_range(5, 7)
+puts and_range_2(5, 7)
