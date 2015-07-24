df027734565f185077d734a55d810260ba5d49cd
Gerula <iacobandrei@gmail.com>
2015-07-23 22:17:22 -0700

    Valid paren

---
+++ LeetCode/remote/valid_p.cs
@@ -0,0 +1,17 @@
+// https://leetcode.com/problems/valid-parentheses/
+
+public class Solution {
+    public bool IsValid(string s) {
+        string prev = s;
+        while (!String.IsNullOrWhiteSpace(s)) {
+            s = s.Replace("()", String.Empty).
+                Replace("[]", String.Empty).
+                Replace("{}", String.Empty);
+            if (s == prev) {
+                return false;
+            }
+            prev = s;
+        }
+        return true;
+    }
+}
