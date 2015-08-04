e202f69e32e1bf99d5d202a97455f2b53644ee09
Gerula <iacobandrei@gmail.com>
2015-08-03 21:57:27 -0700

    My previous solution was O(N) amortized due to the collisions in the sum-based hash. This one is O(N) worst case becase each element is processed at most twice. It indeed uses 3 hashes, but everything in life is a trade-off between memory and speed

--- CareerCup/anagram_indexes.rb
+++ CareerCup/anagram_indexes.rb
@@ -49,4 +49,43 @@ def anagrams(s, t)
     return result
 end
 
+def anagrams_ninja(s, t)
+    small = s.chars.inject({}) { |acc, c|
+        acc[c] ||= 0
+        acc[c] += 1
+        acc
+    }
+    to_consume = small.dup
+    
+    result = []
+    start = 0
+    found = false
+    large = {}
+    0.upto(t.size - 1).each { |end_idx|
+        c = t[end_idx]
+        unless small[c].nil?
+            large[c] ||= 0
+            large[c] += 1
+        end
+
+        unless found
+             to_consume[c] -= 1 if to_consume[c]
+             to_consume.delete(c) if to_consume[c] && to_consume[c] == 0
+             found = to_consume.size == 0
+        end 
+
+        new_start = false
+        while small[t[start]].nil? || large[t[start]] > small[t[start]]
+            large[t[start]] -= 1 unless small[t[start]].nil?
+            start += 1
+            new_start = true
+        end
+
+        result << start if found && small.size == large.size && (new_start || start == 0)
+    }    
+
+    result
+end
+
 assert_equal([0, 1, 2, 6], anagrams("abc", "bcabcxcba"))
+assert_equal([0, 1, 2, 6], anagrams_ninja("abc", "bcabcxcba"))
