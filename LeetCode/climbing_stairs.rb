3abc181b682766c85afdfd2c1bdd7bfebc157887
Gerula <iacobandrei@gmail.com>
2015-08-16 14:27:36 -0700

    It is Fibonacci

--- LeetCode/climbing_stairs.rb
+++ LeetCode/climbing_stairs.rb
@@ -1,26 +1,21 @@
+# https://leetcode.com/submissions/detail/36531864/
+
 require 'test/unit'
 extend Test::Unit::Assertions
 
 def climb_stairs(n)
-    result = [0]
-    climb(1, n, result)
-    return result.first
+    result = [0, 1, 2]
+    3.upto(n).each { |i|
+        result[i] = result[i - 1] + result [i - 2]
+    }
+    return result[n]
 end
 
-def climb(current, n, result)
-    if current == n
-        result[0] += 1
-        return
-    end
-
-    climb(current + 1, n, result) if current + 1 <= n
-    climb(current + 2, n, result) if current + 2 <= n
-end
-
-assert_equal(3, climb_stairs(4))
-assert_equal(5, climb_stairs(5))
-assert_equal(8, climb_stairs(6))
-assert_equal(13, climb_stairs(7))
-assert_equal(21, climb_stairs(8))
-assert_equal(34, climb_stairs(9))
-assert_equal(55, climb_stairs(10))
+assert_equal(2, climb_stairs(2))
+assert_equal(5, climb_stairs(4))
+assert_equal(8, climb_stairs(5))
+assert_equal(13, climb_stairs(6))
+assert_equal(21, climb_stairs(7))
+assert_equal(34, climb_stairs(8))
+assert_equal(55, climb_stairs(9))
+assert_equal(89, climb_stairs(10))
