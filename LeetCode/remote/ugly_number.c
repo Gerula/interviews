3570690f0ed56a9ce170547ac2550f594d6ebafe
Gerula <iacobandrei@gmail.com>
2015-08-23 17:50:31 -0700

    Ugly number

---
+++ LeetCode/remote/ugly_number.c
@@ -0,0 +1,17 @@
+bool isUgly(int num) {
+    if (num == 0) {
+        return false;
+    }
+    
+    if (num == 1) {
+        return true;
+    }
+    
+    for (int i = 2; i < 6; i++) {
+        while (num % i == 0) {
+            num /= i;
+        }
+    }
+    
+    return num == 1;
+}
