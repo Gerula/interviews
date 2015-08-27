e3770057f6d5970c5b758da68b23aecd9bbe9bea
Gerula <iacobandrei@gmail.com>
2015-08-26 19:09:01 -0700

    Faster version

--- LeetCode/remote/integer_to_roman.rb
+++ LeetCode/remote/integer_to_roman.rb
@@ -38,8 +38,74 @@ def int_to_roman(num)
     }
 end
 
+# https://leetcode.com/submissions/detail/37734695/
+#
+# 3999 / 3999 test cases passed.
+#   Status: Accepted
+#   Runtime: 168 ms
+#       
+#       Submitted: 0 minutes ago
+#
+# You are here!
+# Your runtime beats 100.00% of ruby coders. <- sure looks ugly but it's fucking fast!
+#
+#
+
+def int_to_roman_2(num)
+    result = ""
+    while num > 0
+        if num >= 1000
+            num -= 1000
+            result += "M"
+        elsif num >= 900
+            num -= 900
+            result += "CM"
+        elsif num >= 500
+            num -= 500
+            result += "D"
+        elsif num >= 400
+            num -= 400
+            result += "CD"
+        elsif num >= 100
+            num -= 100
+            result += "C"
+        elsif num >= 90
+            num -= 90
+            result += "XC"
+        elsif num >= 50
+            num -= 50
+            result += "L"
+        elsif num >= 40
+            num -= 40
+            result += "XL"
+        elsif num >= 10
+            num -= 10
+            result += "X"
+        elsif num >= 9
+            num -= 9
+            result += "IX"
+        elsif num >= 5
+            num -= 5
+            result += "V"
+        elsif num >= 4
+            num -= 4
+            result += "IV"
+        else
+            num -= 1
+            result += "I"
+        end
+    end
+
+    return result
+end
+
 assert_equal("V", int_to_roman(5))
 assert_equal("MCMLIV", int_to_roman(1954))
 assert_equal("MCMXC", int_to_roman(1990))
 assert_equal("MMXIV", int_to_roman(2014))
 assert_equal("MMMCMXCIX", int_to_roman(3999))
+assert_equal("V", int_to_roman_2(5))
+assert_equal("MCMLIV", int_to_roman_2(1954))
+assert_equal("MCMXC", int_to_roman_2(1990))
+assert_equal("MMXIV", int_to_roman_2(2014))
+assert_equal("MMMCMXCIX", int_to_roman_2(3999))
