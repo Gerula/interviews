9577db4bec56f57cecf1513c357c2fc7337b0c40
Gerula <iacobandrei@gmail.com>
2015-08-29 19:03:49 -0700

    Simple version - power of 3

--- CareerCup/power_of_3.rb
+++ CareerCup/power_of_3.rb
@@ -3,27 +3,20 @@ extend Test::Unit::Assertions
 
 class Fixnum
     def pow_3?
-        chars = self.to_s.chars
-        return true if self == 3 || self == 0
-        return (chars.last == '1' ||
-                chars.last == "3" ||
-                chars.last == "9" ||
-                chars.last == "7") &&
-                chars.map { |c| c.to_i }.reduce(:+) % 9 == 0
+        return 3486784401 % self == 0
     end
 
     def pow_3_n?
         x = self
 
-        while x > 1
-            return false if x % 3 != 0
+        while x % 3 == 0
             x /= 3
         end
 
-        return true
+        return x == 1
     end
 end
 
-3.upto(2 ** (0.size * 2)).each { |i|
+3.upto(34867).each { |i|
         assert_equal(i.pow_3_n?, i.pow_3?, "For #{i}")
 }
