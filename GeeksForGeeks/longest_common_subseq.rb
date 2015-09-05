bc348eb16d350d84541107c0c3d15e0b3edc89d2
Gerula <iacobandrei@gmail.com>
2015-09-04 23:33:22 -0700

    Longest common subseq DP

--- GeeksForGeeks/longest_common_subseq.rb
+++ GeeksForGeeks/longest_common_subseq.rb
@@ -7,5 +7,29 @@ def lcs_size(a, b)
     return [lcs_size(a[0...-1], b), lcs_size(a, b[0...-1])].max
 end
 
+def lcs_size_2(a, b)
+    dp = Array.new(a.size + 1) {
+        Array.new(b.size + 1) {
+            0
+        }
+    }
+
+    for i in 0..a.size
+        for j in 0..b.size
+            if i == 0 || j == 0
+                dp[i][j] = 0
+            elsif a[i - 1] == b[j - 1]
+                dp[i][j] = 1 + dp[i - 1][j - 1]
+            else
+                dp[i][j] = [dp[i - 1][j], dp[i][j - 1]].max
+            end
+        end
+    end
+    
+    return dp[a.size][b.size]
+end
+
 assert_equal("ADH".size, lcs_size("ABCDGH", "AEDFHR"))
 assert_equal("GTAB".size, lcs_size("AGGTAB", "GXTXAYB"))
+assert_equal("ADH".size, lcs_size_2("ABCDGH", "AEDFHR"))
+assert_equal("GTAB".size, lcs_size_2("AGGTAB", "GXTXAYB"))
