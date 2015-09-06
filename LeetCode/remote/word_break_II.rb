96d3dc471030fedeeab0fc492269674cb89581a5
Gerula <iacobandrei@gmail.com>
2015-09-05 21:10:28 -0700

    Word break backtracking with pruning

--- LeetCode/remote/word_break_II.rb
+++ LeetCode/remote/word_break_II.rb
@@ -4,7 +4,15 @@
 require 'test/unit'
 extend Test::Unit::Assertions
 
-# This approach exceeds time limit for test #2
+# Submission Details
+# 29 / 29 test cases passed.
+#   Status: Accepted
+#   Runtime: 132 ms
+#       
+#       Submitted: 0 minutes ago
+# You are here!
+# Your runtime beats 50.00% of ruby submissions. - Even Steven
+#
 def word_break(s, word_dict)
     result = []
     dictionary = word_dict.inject({}) { |acc, x|
@@ -12,12 +20,24 @@ def word_break(s, word_dict)
         acc
     }
 
-    word_break_helper(s, 0, "", result, dictionary)
+    n = s.size
+    can_break = Array.new(n + 1) { false }
+    can_break[n] = true
+    (n - 1).downto(0).each { |i|
+        i.upto(n - 1).each { |j|
+            if dictionary[s[i...(j + 1)]] && can_break[j + 1]
+                can_break[i] = true
+                break
+            end
+        }
+    }
+
+    word_break_helper(s, 0, "", result, dictionary, can_break)
 
     return result
 end
 
-def word_break_helper(s, index, partial, result, dictionary)
+def word_break_helper(s, index, partial, result, dictionary, can_break)
     if index == s.size
         result << partial
         return
@@ -31,7 +51,8 @@ def word_break_helper(s, index, partial, result, dictionary)
                           i + 1,
                           partial + (partial.empty? ? "" : " ") + word,
                           result,
-                          dictionary) if dictionary[word]
+                          dictionary,
+                          can_break) if dictionary[word] and can_break[i + 1]
         i += 1
     end
 end
@@ -40,6 +61,6 @@ assert_equal(["cats and dog", "cat sand dog"].sort,
              word_break("catsanddog",
                         ["cat", "cats", "and", "sand", "dog"]).sort)
  
-assert_equal(["cats and dog", "cat sand dog"].sort,
+assert_equal([].sort,
              word_break("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaab",
                         ["a","aa","aaa","aaaa","aaaaa","aaaaaa","aaaaaaa","aaaaaaaa","aaaaaaaaa","aaaaaaaaaa"]).sort)
