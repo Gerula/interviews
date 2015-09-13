829f86b041659eba7a0e675ed1517e164e6b61b5
Gerula <iacobandrei@gmail.com>
2015-09-12 22:16:40 -0700

    Add digits - good solution - digital root congruence formula

--- LeetCode/remote/add_digits.rb
+++ LeetCode/remote/add_digits.rb
@@ -8,4 +8,12 @@ def add_digits(num)
     return num
 end
 
+def add_digits_2(num)
+    return 0 if num == 0
+    return 9 if num % 9 == 0
+    return eum % 9
+end
+
 puts add_digits(38)
+puts add_digits_2(38)
+
