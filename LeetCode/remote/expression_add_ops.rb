dbacc118d1d8d658dfd4b0cf260d850d6f12dc50
Gerula <iacobandrei@gmail.com>
2015-09-16 20:02:45 -0700

    Final working version

--- LeetCode/remote/expression_add_ops.rb
+++ LeetCode/remote/expression_add_ops.rb
@@ -2,6 +2,16 @@
 #  Given a string that contains only digits 0-9 and a target value,
 #  return all possibilities to add operators +, -, or *
 #  between the digits so they evaluate to the target value. 
+# 
+# 
+# Submission Details
+# 19 / 19 test cases passed.
+#   Status: Accepted
+#   Runtime: 1424 ms <- Holy shit! :)
+#       
+#       Submitted: 0 minutes ago
+#
+# This was unexpectedly hard..
 
 require 'test/unit'
 extend Test::Unit::Assertions
@@ -9,47 +19,33 @@ extend Test::Unit::Assertions
 def add_operators(num, target)
     result = []
     return result if num.size < 2
-    build_expression(num.chars, 1, target, num.chars.first, result)
+    build_expression(num, 0, 0, target, "", result, 0)
     return result
 end
 
-def evaluate(string)
-    if string.size == 1
-        return string.to_i
-    else
-        return operator(string[0], string[1], evaluate(string[2..-1]))
-    end
-end
-
-def operator(left ,op, right)
-    case op
-        when "*"
-            return left.to_i * right.to_i
-        when "+"
-            return left.to_i + right.to_i
-        when "-"
-            return left.to_i - right.to_i
-    end
-end
-
-def build_expression(num, index, target, expression, result) 
+def build_expression(num, index, value, target, expression, result, prev) 
     if index == num.size
-        result << expression if evaluate(expression) == target
+        result << expression if value == target
         return
     end
 
-    current = num[index].to_i
-    ["+", "-", "*"].each { |op|
-            build_expression(num, 
-                            index + 1, 
-                            target, 
-                            "#{expression}#{op}#{current}",
-                            result)
-    }
+    for i in index...num.size
+        break if i != index && num[index] == "0"
+        current = num[index..i].to_i
+
+        if index == 0
+            build_expression(num, i + 1, current, target, expression + current.to_s,result, current.to_i)
+        else
+            build_expression(num, i + 1, value + current, target, "#{expression}+#{current}", result, current)
+            build_expression(num, i + 1, value - current, target, "#{expression}-#{current}", result, -current)
+            build_expression(num, i + 1, value - prev + prev * current, target, "#{expression}*#{current}", result, prev * current)
+        end
+    end
 end
 
-assert_equal(["1+2+3", "1*2*3"], add_operators("123", 6))
-assert_equal(["2*3+2", "2+3*2"], add_operators("232", 8))
-assert_equal(["0+0", "0-0", "0*0"], add_operators("00", 0))
+assert_equal(["1+2+3", "1*2*3"].sort, add_operators("123", 6).sort)
+assert_equal(["2*3+2", "2+3*2"].sort, add_operators("232", 8).sort)
+assert_equal(["0+0", "0-0", "0*0"].sort, add_operators("00", 0).sort)
+assert_equal(["1*2*3*4*5-6-78+9","1*2*3*4+5+6-7+8+9","1*2*3+4+5+6+7+8+9","1*2*3+4+5-6*7+8*9","1*2*3+4-5*6+7*8+9","1*2*3+4-5*6-7+8*9","1*2*3-4*5+6*7+8+9","1*2*3-4*5-6+7*8+9","1*2*3-4*5-6-7+8*9","1*2*3-45+67+8+9","1*2*34+56-7-8*9","1*2*34-5+6-7-8-9","1*2+3*4-56+78+9","1*2+3+4+5*6+7+8-9","1*2+3+4-5+6*7+8-9","1*2+3+4-5-6+7*8-9","1*2+3+45+67-8*9","1*2+3-45+6+7+8*9","1*2+34+5-6-7+8+9","1*2+34+56-7*8+9","1*2+34-5+6+7-8+9","1*2+34-56+7*8+9","1*2+34-56-7+8*9","1*2-3*4+5+67-8-9","1*2-3+4-5-6*7+89","1*2-3-4*5+67+8-9","1*2-3-4+56-7-8+9","1*2-34+5*6+7*8-9","1*23+4*5-6+7-8+9","1*23-4-56-7+89","1+2*3*4*5+6+7-89","1+2*3*4+5*6+7-8-9","1+2*3*4-5+6*7-8-9","1+2*3+4*5*6+7-89","1+2*3+4*5-6+7+8+9","1+2*3-4-5-6*7+89","1+2*34-5*6+7+8-9","1+2+3*4*5+6-7-8-9","1+2+3*4+5+6*7-8-9","1+2+3*45-6-78-9","1+2+3+4+5+6+7+8+9","1+2+3+4+5-6*7+8*9","1+2+3+4-5*6+7*8+9","1+2+3+4-5*6-7+8*9","1+2+3-4*5+6*7+8+9","1+2+3-4*5-6+7*8+9","1+2+3-4*5-6-7+8*9","1+2+3-45+67+8+9","1+2-3*4*5+6+7+89","1+2-3*4+5*6+7+8+9","1+2-3*4-5+6*7+8+9","1+2-3*4-5-6+7*8+9","1+2-3*4-5-6-7+8*9","1+2-3+4*5+6*7-8-9","1+2-3+45+6-7-8+9","1+2-3+45-6+7+8-9","1+2-3-4-5*6+7+8*9","1+2-3-45-6+7+89","1+2-34+5+6+7*8+9","1+2-34+5+6-7+8*9","1+2-34-5-6+78+9","1+23*4+5-6-7*8+9","1+23*4-5-6*7+8-9","1+23*4-56+7-8+9","1+23+4+5+6+7+8-9","1+23+4-5*6+7*8-9","1+23+4-5-67+89","1+23-4*5+6*7+8-9","1+23-4*5-6+7*8-9","1+23-4-5+6+7+8+9","1+23-4-5-6*7+8*9","1+23-45+67+8-9","1-2*3*4+5-6+78-9","1-2*3*4-5-6+7+8*9","1-2*3+4*5+6+7+8+9","1-2*3+4*5-6*7+8*9","1-2*3+4+5+6*7+8-9","1-2*3+4+5-6+7*8-9","1-2*3+4+56+7-8-9","1-2*3+45-67+8*9","1-2*3-4+5*6+7+8+9","1-2*3-4-5+6*7+8+9","1-2*3-4-5-6+7*8+9","1-2*3-4-5-6-7+8*9","1-2*34+5*6-7+89","1-2+3*4*5-6-7+8-9","1-2+3+4-5*6+78-9","1-2+3+45+6-7+8-9","1-2+3-4*5-6+78-9","1-2+3-45+6-7+89","1-2-3*4+5+6+7*8-9","1-2-3*4-5-6+78-9","1-2-3+4-5+67-8-9","1-2-3+45-6-7+8+9","1-2-34+5+6+78-9","1-2-34+56+7+8+9","1-2-34-5+6+7+8*9","1-23*4+5+6*7+89","1-23+4*5-6*7+89","1-23+4-5+67-8+9","1-23+45-67+89","1-23-4+5+67+8-9","1-23-4-5-6-7+89","12*3*4-5*6-78+9","12*3+4+5+6-7-8+9","12*3+4+5-6+7+8-9","12*3-4-5-6+7+8+9","12*3-4-56+78-9","12+3*4+5+6-7+8+9","12+3*45-6-7-89","12+3+4-56-7+89","12+3-4*5+67-8-9","12+3-45+6+78-9","12+34-5-6-7+8+9","12-3*4*5+6+78+9","12-3*4-5+67-8-9","12-3+4*5+6-7+8+9","12-3+4+56-7-8-9","12-3-4+5*6-7+8+9","12-3-4-56+7+89","12-3-45-6+78+9"].sort, add_operators("123456789", 45).sort)
 assert_equal([], add_operators("3456237490", 9191))
 
