afb5c15d8e7dffee2f14c4fbd1815db4f38d5c2e
Gerula <iacobandrei@gmail.com>
2015-09-25 18:51:16 -0700

    Actual correct solution

--- LeetCode/remote/word_search_II.rb
+++ LeetCode/remote/word_search_II.rb
@@ -6,6 +6,13 @@
 #  cells are those horizontally or vertically neighboring.
 #  
 #  The same letter cell may not be used more than once in a word. 
+# 
+# Submission Details
+# 35 / 35 test cases passed.
+#   Status: Accepted
+#   Runtime: 844 ms
+#       
+#       Submitted: 0 minutes ago
 #
 
 require 'test/unit'
@@ -14,14 +21,16 @@ extend Test::Unit::Assertions
 def find_words(board, words)
     trie = words.inject({}){ |acc_trie, word|
         word.chars.each_with_index.inject(acc_trie) { |acc, x|
-            acc[x[0]] = {:link => {}, :is_word => x[1] == word.size - 1}
+            acc[x[0]] ||= {:link => {}, :is_word => false}
+            acc[x[0]][:is_word] ||= x[1] >= word.size - 1 
             acc[x[0]][:link]
         }
 
         acc_trie
     }
-
+    
     result = []
+
     for i in 0...board.size
         for j in 0...board[0].size
             search(i, j, board, trie, "", result)
@@ -35,21 +44,34 @@ def search(i, j, board, trie, partial, result)
     return if !i.between?(0, board.size - 1) || !j.between?(0, board[0].size - 1)
     current = board[i][j]
     return if trie[current].nil?
-    result << partial + current if trie[current][:is_word]
-    search(i + 1, j, board, trie[current][:link], partial + current, result)
-    search(i, j + 1, board, trie[current][:link], partial + current, result)
-    search(i - 1, j, board, trie[current][:link], partial + current, result)
-    search(i, j - 1, board, trie[current][:link], partial + current, result)
+    partial = partial + current
+    result << partial if trie[current][:is_word] && !result.include?(partial)
+    board[i][j] = "$"
+    search(i + 1, j, board, trie[current][:link], partial, result)
+    search(i, j + 1, board, trie[current][:link], partial, result)
+    search(i - 1, j, board, trie[current][:link], partial, result)
+    search(i, j - 1, board, trie[current][:link], partial, result)
+    board[i][j] = current
 end
 
 assert_equal(["eat","oath"].sort,
              find_words(
-             [
-              ['o','a','a','n'],
-              ['e','t','a','e'],
-              ['i','h','k','r'],
-              ['i','f','l','v']
-             ],
+              ["oaan",
+              "etae",
+              "ihkr",
+              "iflv"],
             ["oath","pea","eat","rain"]).sort)
 
+assert_equal(["aaaaaaaaaaaa"].sort,
+             find_words(
+             ["aaaa",
+              "aaaa",
+              "aaaa"],
+            ["aaaaaaaaaaaa"]).sort)
+
+assert_equal(["ab","ac","bd","ca","db"].sort,
+             find_words(
+             ["ab",
+              "cd"],
+             ["ab","cb","ad","bd","ac","ca","da","bc","db","adcb","dabc","abb","acb"]).sort)
 
