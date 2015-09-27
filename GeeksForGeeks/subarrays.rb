97d4ca3408d8fe34af5da83b0f978c0273db1e04
Gerula <iacobandrei@gmail.com>
2015-09-27 15:37:26 -0700

    Count subarrays between values - better but still naive

--- GeeksForGeeks/subarrays.rb
+++ GeeksForGeeks/subarrays.rb
@@ -9,13 +9,21 @@ extend Test::Unit::Assertions
 class Array
     def subarrays(low, high)
         result = 0
-        for i in 0...self.size
-            for j in 1..self.size - i
-                result += 1 if self[i, j].reduce(:+).between?(low, high)
+        pre = self.inject([]) { |acc, x|
+            acc << (acc.size == 0 ? x : acc[-1] + x)
+        }
+
+        post = self.reverse.inject([]) { |acc, x|
+            acc << (acc.size == 0 ? x : acc[-1] + x)
+        }.reverse
+
+        for i in 0...(self.size - 1)
+            for j in (i + 1)...self.size
+                result += 1 if (pre[j] - pre[i] + self[i]).between?(low, high)
             end
         end
 
-        result
+        return result + self.count{ |x| x.between?(low, high) }
     end
 end
 
