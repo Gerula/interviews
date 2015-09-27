c330cf2ad7898006af815b662e224d66c0386fd7
Gerula <iacobandrei@gmail.com>
2015-09-27 15:38:03 -0700

    Count subarrays between values - better but still naive - remove redundancy

--- GeeksForGeeks/subarrays.rb
+++ GeeksForGeeks/subarrays.rb
@@ -13,10 +13,6 @@ class Array
             acc << (acc.size == 0 ? x : acc[-1] + x)
         }
 
-        post = self.reverse.inject([]) { |acc, x|
-            acc << (acc.size == 0 ? x : acc[-1] + x)
-        }.reverse
-
         for i in 0...(self.size - 1)
             for j in (i + 1)...self.size
                 result += 1 if (pre[j] - pre[i] + self[i]).between?(low, high)
