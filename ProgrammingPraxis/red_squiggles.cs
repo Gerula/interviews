7692a31d4a329eaa0c6256b21307ab9a37f45041
Gerula <iacobandrei@gmail.com>
2015-10-03 19:44:38 -0700

    Build and print the trie

--- ProgrammingPraxis/red_squiggles.cs
+++ ProgrammingPraxis/red_squiggles.cs
@@ -13,7 +13,39 @@ class Trie
     private class Node
     {
         internal Dictionary<char, Node> Next = new Dictionary<char, Node>();
-        bool IsWord = false;
+        internal bool IsWord = false;
+        
+        public override String ToString()
+        {
+            return String.Format("[Node.IsWord:{0} {1}]",
+                                 IsWord,
+                                 String.Join(", ", Next.Select(x => 
+                                                               String.Format("{0} => {1}", x.Key, x.Value.ToString()))));
+        }
+    }
+    
+    private readonly Node Root = new Node();
+
+    public void Add(String s)
+    {
+        Node current = Root;
+        for (int i = 0; i < s.Length; i++)
+        {
+            Node next;
+            if (!current.Next.TryGetValue(s[i], out next))
+            {
+                next = new Node();
+                current.Next[s[i]] = next;
+            }
+        
+            next.IsWord = i == s.Length - 1;
+            current = next;
+        }
+    }
+
+    public override String ToString()
+    {
+        return Root.ToString();
     }
 }
 
@@ -27,8 +59,10 @@ class Program
             String line;
             while ((line = reader.ReadLine()) != null)
             {
-                Console.WriteLine(line);
+                trie.Add(line);
             }
         }
+
+        Console.WriteLine(trie);
     }
 }
