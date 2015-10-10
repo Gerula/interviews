1d39ba72ee38865b3c59b2cf38932be310d18b36
Gerula <iacobandrei@gmail.com>
2015-10-09 22:57:07 -0700

    Yeah!!! Hahahahaha! Got it!

--- CareerCup/Google/delete_duplicates_lexicographic.cs
+++ CareerCup/Google/delete_duplicates_lexicographic.cs
@@ -11,6 +11,30 @@ using System.Text;
 
 static class Program
 {
+    static String GoodDeDup(this String s)
+    {
+        var hash = new Dictionary<char, int>();
+        hash[s[s.Length - 1]] = s.Length - 1;
+        for (int i = s.Length - 2; i >= 0; i--)
+        {
+            if (!hash.ContainsKey(s[i]) || s[i] < s[i + 1] && hash[s[i + 1]] == i + 1)
+            {
+                hash[s[i]] = i;
+            }
+        }
+
+        var result = new StringBuilder();
+        for (int i = 0; i < s.Length; i++)
+        {
+            if (hash[s[i]] == i)
+            {
+                result.Append(s[i]);
+            }
+        }
+
+        return result.ToString();
+    }
+
     static String DeDup(this String s)
     {
         var hash = new Dictionary<char, int>();
@@ -40,9 +64,9 @@ static class Program
             Tuple.Create("bcabc", "abc"),
             Tuple.Create("cbacdcbc", "acdb")
         }.ToList().ForEach(x => {
-                    if (x.Item1.DeDup() != x.Item2) 
+                    if (x.Item1.GoodDeDup() != x.Item2) 
                     {
-                        throw new Exception(String.Format("You're not good enough, look : {0}={1}", x.Item1, x.Item1.DeDup()));
+                        throw new Exception(String.Format("You're not good enough, look : {0}={1}", x.Item1, x.Item1.GoodDeDup()));
                     }
                 });
 
