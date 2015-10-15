036751473bb6a5e76a39d3447de2d8f1dbe92653
Gerula <iacobandrei@gmail.com>
2015-10-14 18:07:24 -0700

    Shortest unique prefix - scaffolding

---
+++ GeeksForGeeks/shortest_uniq_prefix.cs
@@ -0,0 +1,55 @@
+// Given an array of words, find all shortest unique prefixes to represent each word in the given array. Assume that no word is prefix of another. 
+//
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class WordProcessor
+{
+    private class Node 
+    {
+        public readonly Dictionary<char, Node> Next = new Dictionary<char, Node>();
+        public int Stamp = 0;
+    }
+
+    private Node Root;
+
+    public WordProcessor()
+    {
+        Root = new Node();    
+    }
+
+    public void Add(String word)
+    {
+        var current = Root;
+        foreach (char c in word)
+        {
+            if (!current.Next.ContainsKey(c)) 
+            {
+                current.Next[c] = new Node();
+            }
+
+            current.Stamp++;
+            current = current.Next[c];
+        }
+    }
+
+    public String Prefix(String word)
+    {
+        return word;
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        var processor = new WordProcessor();
+        var words = new List<String> {"zebra", "dog", "duck", "dove"};
+        words.ForEach(processor.Add);
+        Console.WriteLine(String.Join(
+                                    Environment.NewLine,
+                                    words.Select(x => String.Format("{0} = {1}", x, processor.Prefix(x)))));
+    }
+}
