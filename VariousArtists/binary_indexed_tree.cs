87c56f56e2a80062ed67ec453b8867d0000f6ad4
Gerula <iacobandrei@gmail.com>
2015-10-18 11:20:29 -0700

    Binary indexed trees - as easy as it is to write it, as hard is to understand it

--- VariousArtists/binary_indexed_tree.cs
+++ VariousArtists/binary_indexed_tree.cs
@@ -2,6 +2,56 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 
+class Bulbs
+{
+    private int[] a;
+
+    public Bulbs(int max)
+    {
+        a = new int[max];
+    }
+
+    int Read(int i)
+    {
+        int result = 0;
+        while (i > 0) 
+        {
+            result += a[i];
+            i -= (i & -i);
+        }
+
+        return result;
+    }
+
+    void Update(int i, int val)
+    {
+        while (i < a.Length)
+        {
+            a[i] += val;
+            i += (i & -i);
+        }
+    }
+
+    public bool Get(int i)
+    {
+        return Read(i) % 2 == 1;
+    }
+
+    public Bulbs Toggle(int i, int j)
+    {
+        Update(i, 1);
+        Update(j + 1, -1);
+        return this;
+    }
+
+    public override String ToString()
+    {
+        return String.Join(
+                String.Empty,
+                a.Select((x, i) => Get(i) ? "X" : " "));
+    }
+}
+
 static class Program 
 {
     static IEnumerable<String> Decompose(this int n)
@@ -21,12 +71,12 @@ static class Program
 
     static void Main()
     {
-        for (int i = 2; i < 30; i++)
-        {
-            Console.WriteLine(
-                    "{0} = {1}",
-                    i,
-                    String.Join(" + ", i.Decompose()));
-        }
+        var b = new Bulbs(40);
+        b.
+        Toggle(2, 10).
+        Toggle(5, 15).
+        Toggle(7, 30).
+        Toggle(8, 20);
+        Console.WriteLine(b);
     }
 }
