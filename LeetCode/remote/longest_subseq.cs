c47bc7247ace6c8fe4f0d4322378e52fdf236abc
Gerula <iacobandrei@gmail.com>
2015-11-03 07:10:14 -0800

    Construct subseq

--- LeetCode/remote/longest_subseq.cs
+++ LeetCode/remote/longest_subseq.cs
@@ -29,22 +29,40 @@ public class Solution {
         }
 
         var subseq = Enumerable.Repeat(1, nums.Length).ToArray();
+        var pre = Enumerable.Repeat(0, nums.Length).ToArray();
+        pre[0] = -1;
         for (int i = 1; i < nums.Length; i++)
         {
             for (int j = 0; j < i; j++)
             {
                 if (nums[j] < nums[i] && subseq[i] < subseq[j] + 1)
                 {
+                    pre[i] = j;
                     subseq[i] = subseq[j] + 1;
                 }
             }
         }
 
-        return subseq.Max();
+        var max = subseq.
+                    Select((item, index) => new { item, index }).
+                    OrderBy(x => x.item).
+                    Last().index;
+
+        var it = max;
+        while (it > 0)
+        {
+            Console.Write("{0} ", nums[it]);
+            it = pre[it];
+        }
+
+        Console.WriteLine();
+
+        return subseq[max];
     }
 
     static void Main()
     {
         Console.WriteLine(LengthOfLIS(new [] {10, 9, 2, 5, 3, 7, 101, 18}));
+        Console.WriteLine(LengthOfLIS(new [] {1,3,6,7,9,4,10,5,6}));
     }
 }
