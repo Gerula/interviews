e6da43b62a2ff7fd31c01a39cc1cab5e6297ff75
Gerula <iacobandrei@gmail.com>
2015-11-10 17:43:28 -0800

    Random 7 using Random 5 - bit version - shitty

--- VariousArtists/random_7_5.cs
+++ VariousArtists/random_7_5.cs
@@ -26,11 +26,39 @@ class Program
         }[Rand5() - 1, Rand5() - 1];
     }
 
+    static int Rand72()
+    {
+        return 1 * (Rand5() <= 2 ? 0 : 1) +
+               2 * (Rand5() == 2) +
+               4 * (Rand5() >= 3 ? 1 : 0);
+    }
+
     static void Main()
     {
         Enumerable.
             Repeat(1, 7000000).
             Select(x => Rand7()).
+            Aggregate(
+                    new Dictionary<int, int>(),
+                    (acc, y) => {
+                        if (!acc.ContainsKey(y))
+                        {
+                            acc[y] = 0;
+                        }
+
+                        acc[y]++;
+                        return acc;
+                    }).
+            OrderBy(z => z.Key).
+            ToList().
+            ForEach(w => {
+                        Console.WriteLine("{0} = {1}", w.Key, w.Value);
+                    });
+
+        Console.WriteLine();
+        Enumerable.
+            Repeat(1, 7000000).
+            Select(x => Rand72()).
             Aggregate(
                     new Dictionary<int, int>(),
                     (acc, y) => {
