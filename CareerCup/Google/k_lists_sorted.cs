da13336129cfa849a7e555609eb7ccc108c7edc0
Gerula <iacobandrei@gmail.com>
2015-11-19 07:20:48 -0800

    Smallest interval - first atteempt

---
+++ CareerCup/Google/k_lists_sorted.cs
@@ -0,0 +1,69 @@
+// http://careercup.com/question?id=16759664
+//
+// K lists of sorted ints. Find the smallest range that includes at least one element from 
+// each of the list.
+//
+// For example:
+// { 4, 10, 15, 24, 26 }
+// { 0, 9, 12, 20 }
+// { 3, 18, 22, 30 }
+//
+// result : [20, 24].
+//
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+static class Program
+{
+    static Tuple<int, int> Interval(this int[][] lists)
+    {
+        var first = 0;
+        var second = 0;
+        var k = lists.Length;
+        var visited = new HashSet<int>();
+        var indexes = new int[k];
+        while (visited.Count < k) 
+        {
+            var min = int.MaxValue;
+            for (var i = 0; i < k; i++) 
+            {
+                min = (indexes[i] < lists[i].Length && lists[i][indexes[i]] < min) ? lists[i][indexes[i]] : min;
+            }
+
+            for (var i = 0; i < k; i++) 
+            {
+                if (lists[i][indexes[i]] == min) 
+                {
+                    if (!visited.Contains(i)) 
+                    {
+                        visited.Add(i);
+                        if (visited.Count == k - 1)
+                        {
+                            first = min;
+                        }
+
+                        if (visited.Count == k)
+                        {
+                            second = min;
+                        }
+                    }
+
+                    indexes[i]++;
+                }
+            }
+        }
+
+        return Tuple.Create(first, second);
+    }
+
+    static void Main()
+    {
+        Console.WriteLine(new [] {
+                    new [] { 4, 10, 15, 24, 26},
+                    new [] { 0, 9, 12, 20 },
+                    new [] { 3, 18, 22, 30 }
+                }.Interval());
+    }
+}
