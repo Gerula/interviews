f50266cf628692d8f62a4b66220c6a233172bde8
Gerula <iacobandrei@gmail.com>
2015-11-27 20:12:46 -0800

    with cache

--- LeetCode/remote/scramble_string.cs
+++ LeetCode/remote/scramble_string.cs
@@ -4,27 +4,41 @@
 //
 // First attempt:
 //
+// 
 // Submission Details
 // 281 / 281 test cases passed.
 //  Status: Accepted
-//  Runtime: 164 ms
+//  Runtime: 160 ms
 //      
 //      Submitted: 0 minutes ago
-//
 
 using System;
+using System.Collections.Generic;
 using System.Linq;
 
 public class Solution {
     public bool IsScramble(string s1, string s2) {
+        return IsScrambleHelper(s1, s2, new Dictionary<String, bool>());
+    }
+
+    public bool IsScrambleHelper(string s1, string s2, Dictionary<String, bool> cache)
+    {
+        var key = s1 + "." + s2;
+        if (cache.ContainsKey(key))
+        {
+            return cache[key];
+        }
+
         if (s1 == s2)
         {
-            return true;
+            cache[key] = true;
+            return cache[key];
         }
 
         if (!s1.OrderBy(x => x).SequenceEqual(s2.OrderBy(x => x)))
         {
-            return false;
+            cache[key] = false;
+            return cache[key];
         }
 
         for (var i = 1; i < s1.Length; i++)
@@ -35,18 +49,21 @@ public class Solution {
             var s22 = s2.Substring(i);
             if (IsScramble(s11, s21) && IsScramble(s12, s22))
             {
-                return true;
+                cache[key] = true;
+                return cache[key];
             }
 
             s21 = s2.Substring(0, s2.Length - i);
             s22 = s2.Substring(s2.Length - i);
             if (IsScramble(s11, s22) && IsScramble(s12, s21))
             {
-                return true;
+                cache[key] = true;
+                return cache[key];
             }
         }
 
-        return false;
+        cache[key] = false;
+        return cache[key];
     }
 
     static void Main()
