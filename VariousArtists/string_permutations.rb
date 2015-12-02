fe76f8bae76c087e21e7f7e5cdb2cfd84c45dc3d
Gerula <iacobandrei@gmail.com>
2015-12-01 21:47:53 -0800

    Golfing

--- VariousArtists/string_permutations.rb
+++ VariousArtists/string_permutations.rb
@@ -3,16 +3,11 @@
 class String
     def permute
         return [self] if self.size == 1
-        permutations = self[1..-1].permute
-        char = self[0]
-        result = []
-        permutations.each { |x|
-            for i in 0..self[1..-1].size
-                result << "#{x[0, i]}#{char}#{x[i..-1]}"
-            end
-        }
-
-        result
+        self[1..-1].permute.map { |x|
+            (0...self.size).map { |i|
+                "#{x[0, i]}#{self[0]}#{x[i..-1]}"
+            }
+        }.flatten
     end
 end
 
