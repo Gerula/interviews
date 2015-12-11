3ba68fd166d4862e5b231d0046ff0d7a8307bb58
Gerula <iacobandrei@gmail.com>
2015-12-10 19:02:56 -0800

    Decode ways - smarter backtrack

--- LeetCode/remote/decode_ways.cs
+++ LeetCode/remote/decode_ways.cs
@@ -17,34 +17,37 @@
 using System;
 
 public class Solution {
+    // "9371597631128776948387197132267188677349946742344217846154932859125134924241649584251978418763151253"
+    //
+    // Time limit exceeded - lol of course
     public int NumDecodings(string s) {
-        var num = 0;
-        CountDecoding(s, ref num);
-        return num;
-    }
-
-    public void CountDecoding(string s, ref int num)
-    {
         if (String.IsNullOrEmpty(s))
         {
-            num++;
-            return;
+            return 1;
         }
 
+        var sum = 0;
         if (s[0] != '0')
         {
-            CountDecoding(s.Substring(1), ref num);
+            sum += NumDecodings(s.Substring(1));
         }
 
-        if (s.Length > 1 && int.Parse(s.Substring(0, 2)) < 26)
+        if (s.Length > 1 && int.Parse(s.Substring(0, 2)) < 27)
         {
-            CountDecoding(s.Substring(2), ref num);
+            sum += NumDecodings(s.Substring(2));
         }
+
+        return sum;
     }
 
     static void Main()
     {
         var s = new Solution();
+        // 1 2 2 6
+        // 12 2 6
+        // 12 26
+        // 1 2 26
+        // 1 22 6
         foreach (var t in new [] { "12", "1226" })
         {
             Console.WriteLine("{0} - {1}", t, s.NumDecodings(t));
