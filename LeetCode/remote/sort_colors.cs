dff20579be2cbeb423e78943f001ab3b9e03d46d
Gerula <iacobandrei@gmail.com>
2015-12-13 20:53:35 -0800

    Sort colors

---
+++ LeetCode/remote/sort_colors.cs
@@ -0,0 +1,66 @@
+//  https://leetcode.com/problems/sort-colors/
+//
+//   Given an array with n objects colored red, white or blue, sort them so that objects of the same color are adjacent,
+//   with the colors in the order red, white and blue.
+//
+//   Here, we will use the integers 0, 1, and 2 to represent the color red, white, and blue respectively. 
+
+using System;
+using System.Linq;
+
+public class Solution {
+    //  https://leetcode.com/submissions/detail/48073928/
+    //
+    //
+    //  Submission Details
+    //  86 / 86 test cases passed.
+    //      Status: Accepted
+    //      Runtime: 540 ms
+    //          
+    //          Submitted: 0 minutes ago
+    //
+    public static void SortColors(int[] nums) {
+        var low = 0;
+        var intermediate = 0;
+        var high = nums.Length;
+        while (intermediate < high)
+        {
+            switch (nums[intermediate])
+            {
+                case 0: 
+                    Swap(ref nums[intermediate], ref nums[low]);
+                    low++;
+                    intermediate++;
+                    break;
+                case 1:
+                    intermediate++;
+                    break;
+                case 2:
+                    high--;
+                    Swap(ref nums[intermediate], ref nums[high]);
+                    break;
+            }
+        }
+    }
+
+    public static void Swap(ref int a, ref int b)
+    {
+        var c = a;
+        a = b;
+        b = c;
+    }
+
+    static void Main()
+    {
+        var rand = new Random();
+        var a = Enumerable.Repeat(1, 97).Select(x => rand.Next(3)).ToArray();
+        Console.WriteLine(String.Join(
+                    String.Empty, 
+                    a));
+        SortColors(a);
+        Console.WriteLine(String.Join(
+                    String.Empty, 
+                    a));
+    }
+}
+
