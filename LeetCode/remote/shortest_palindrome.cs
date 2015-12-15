cb64e143d42d24faeae1cfc06d8c9e8352c8f133
Gerula <iacobandrei@gmail.com>
2015-12-14 18:15:32 -0800

    Brilliant!

--- LeetCode/remote/shortest_palindrome.cs
+++ LeetCode/remote/shortest_palindrome.cs
@@ -45,12 +45,72 @@ public class Solution {
         return true;
     }
 
+    //  
+    //  Submission Details
+    //  117 / 117 test cases passed.
+    //      Status: Accepted
+    //      Runtime: 128 ms
+    //          
+    //          Submitted: 0 minutes ago
+    //
+    public String ShortestPalindrome2(string s) 
+    {
+        long _base = 19;
+        if (String.IsNullOrEmpty(s))
+        {
+            return s;
+        }
+
+        var hashA = new long[s.Length];
+        var hashB = new long[s.Length];
+        hashA[0] = hashB[0] = (long) s[0];
+        var mul = _base;
+        for (var i = 1; i < s.Length; i++)
+        {
+            hashA[i] = hashA[i - 1] * _base + s[i];
+            hashB[i] = hashB[i - 1] + s[i] * mul;
+            mul *= _base;
+        }
+
+        for (var i = s.Length - 1; i >= 0; i--)
+        {
+            if (EqualHash(s, hashA[i], hashB[i], i))
+            {
+                return new String(s.Substring(i + 1, s.Length - i - 1).Reverse().ToArray()) + s;
+            }
+        }
+
+        return String.Empty;
+    }
+
+    public bool EqualHash(String a, long hashA, long hashB, int length)
+    {
+        if (hashA != hashB)
+        {
+            return false;
+        }
+
+        var i = 0;
+        var j = length;
+        while (length > 0)
+        {
+            length--;
+            if (a[i++] != a[j--])
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
     static void Main()
     {
         var s = new Solution();
         foreach (var str in new [] { "abcd", "aacecaaa"})
         {
             Console.WriteLine("{0} - {1}", str, s.ShortestPalindrome(str));
+            Console.WriteLine("{0} - {1}", str, s.ShortestPalindrome2(str));
         }
     }
 }
