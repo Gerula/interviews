a4701f94d9bb03057609d6c0b4673091c3ae59bb
Gerula <iacobandrei@gmail.com>
2015-12-19 10:03:06 -0800

    2nd method

--- LeetCode/remote/remove_list_elements.cs
+++ LeetCode/remote/remove_list_elements.cs
@@ -19,3 +19,31 @@ public class Solution {
         return head.val == val ? head.next : head;
     }
 
+    //  
+    //  Submission Details
+    //  63 / 63 test cases passed.
+    //      Status: Accepted
+    //      Runtime: 172 ms
+    //          
+    //          Submitted: 0 minutes ago
+    //
+    public ListNode RemoveElements(ListNode head, int val) 
+    {
+        var dummy = new ListNode(0) { next = head };
+        var it = dummy;
+
+        while (it != null && it.next != null)
+        {
+            if (it.next.val == val)
+            {
+                it.next = it.next.next;
+            }
+            else
+            {
+                it = it.next;
+            }
+        }
+
+        return dummy.next;
+    }
+}
