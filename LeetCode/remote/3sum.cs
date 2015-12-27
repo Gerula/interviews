c2c47451db893939d6c4ff8c23c6bb762dc25214
Gerula <iacobandrei@gmail.com>
2015-12-26 22:12:51 -0800

    3 sum - fixed

--- LeetCode/remote/3sum.cs
+++ LeetCode/remote/3sum.cs
@@ -18,29 +18,43 @@ using System.Collections.Generic;
 using System.Linq;
 
 public class Solution {
+    //  
+    //  Submission Details
+    //  311 / 311 test cases passed.
+    //      Status: Accepted
+    //      Runtime: 576 ms
+    //          
+    //          Submitted: 0 minutes ago
+    //
+    //          https://leetcode.com/submissions/detail/48992357/
+    //
     public IList<IList<int>> ThreeSum(int[] nums) {
         var sorted = nums.OrderBy(x => x).ToArray();
         var result = new List<IList<int>>();
         for (var i = 0; i < sorted.Length - 1; i++)
         {
-            var low = i + 1;
-            while (low < sorted.Length - 1 && sorted[low] == sorted[low + 1])
+            if (i > 0 && sorted[i] == sorted[i - 1])
             {
-                low++;
-            }
-    
-            var high = sorted.Length - 1;
-            while (high > low && sorted[high] == sorted[high - 1])
-            {
-                high--;
+                continue;
             }
 
+            var low = i + 1;
+            var high = sorted.Length - 1;
             while (low < high)
             {
                 var sum = sorted[i] + sorted[low] + sorted[high];
                 if (sum == 0)
                 {
                     result.Add(new List<int> { sorted[i], sorted[low], sorted[high] });
+                    while (low < sorted.Length - 1 && sorted[low] == sorted[low + 1])
+                    {
+                        low++;
+                    }
+                    while (high > low && sorted[high] == sorted[high - 1])
+                    {
+                        high--;
+                    }
+
                     low++;
                     high--;
                 }
