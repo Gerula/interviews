3a0c0ad0f969e0e138879bbe7a8ffb7cb8d938f3
Gerula <iacobandrei@gmail.com>
2016-01-03 06:37:57 -0800

    Balance - easier method - why do I always forget the two pointers stuff?

--- CareerCup/balance.cs
+++ CareerCup/balance.cs
@@ -31,6 +31,29 @@ static class Program
         return idx;
     }
 
+    static int Balans(this int[] a)
+    {
+        var low = 0;
+        var high = a.Length - 1;
+        var lowSum = 0;
+        var highSum = 0;
+        while (low <= high)
+        {
+            if (lowSum < highSum)
+            {
+                lowSum += a[low];
+                low++;
+            }
+            else
+            {
+                highSum += a[high];
+                high--;
+            }
+        }
+
+        return low;
+    }
+
     static void Main()
     {
         var r = new Random();
@@ -39,10 +62,12 @@ static class Program
             var len = r.Next(5, 15);
             var a = Enumerable.Repeat(1, len).Select(x => (r.Next(1, 10) % 2 == 0 ? -1 : 1) * r.Next(1, 10)).ToArray();
             var idx = a.Balance();
+            var idx2 = a.Balans();
             Console.WriteLine(
-                    "{0} - <{1}> [{2}]={3} [{4}]={5}",
+                    "{0} - <{1}><2> [{3}]={4} [{5}]={6}",
                     String.Join(", ", a),
                     idx,
+                    idx2,
                     String.Join(", ", a.Take(idx + 1)),
                     a.Take(idx + 1).Sum(),
                     String.Join(", ", a.Skip(idx + 1)),
