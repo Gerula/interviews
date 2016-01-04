3e2182ab0ac94144f6b45605aba52c20ce8bf41b
Gerula <iacobandrei@gmail.com>
2016-01-03 17:39:13 -0800

    Max letter A

---
+++ GeeksForGeeks/max_letter_a.cs
@@ -0,0 +1,46 @@
+//  http://www.geeksforgeeks.org/how-to-print-maximum-number-of-a-using-given-four-keys/
+//
+//  This is a famous interview question asked in Google, Paytm and many other company interviews.
+//
+//  Below is the problem statement.
+//
+//  Imagine you have a special keyboard with the following keys: 
+//  Key 1:  Prints 'A' on screen
+//  Key 2: (Ctrl-A): Select screen
+//  Key 3: (Ctrl-C): Copy selection to buffer
+//  Key 4: (Ctrl-V): Print buffer on screen appending it
+//                   after what has already been printed. 
+//
+//  If you can only press the keyboard for N times (with the above four
+//  keys), write a program to produce maximum numbers of A's. That is to
+//  say, the input parameter is N (No. of keys that you can press), the 
+//  output is M (No. of As that you can produce).
+
+using System;
+
+static class Program
+{
+    static int Max(this int n)
+    {
+        if (n <= 6)
+        {
+            return n;
+        }
+
+        var max = int.MinValue;
+        for (var i = n - 3; i >= 1; i--)
+        {
+            max = Math.Max(max, (n - i - 1) * i.Max());
+        }
+
+        return max;
+    }
+
+    static void Main()
+    {
+        foreach (var x in new [] { 3, 7, 11 })
+        {
+            Console.WriteLine("{0} = {1}", x, x.Max());
+        }
+    }
+}
