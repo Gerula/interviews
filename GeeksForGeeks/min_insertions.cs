21477ffbe4ca53f618bb1c037ea6dc35451bb1eb
Gerula <iacobandrei@gmail.com>
2016-01-08 07:40:43 -0800

    Min insertions to make palindrome

---
+++ GeeksForGeeks/min_insertions.cs
@@ -0,0 +1,49 @@
+//  http://www.geeksforgeeks.org/dynamic-programming-set-28-minimum-insertions-to-form-a-palindrome/
+//
+//  Given a string, find the minimum number of characters to be inserted to convert it to palindrome.
+
+using System;
+
+static class Program
+{
+    static int Palind(this String s)
+    {
+        if (String.IsNullOrEmpty(s))
+        {
+            return int.MaxValue;
+        }
+
+        if (s.Length == 1)
+        {
+            return 0;
+        }
+
+        if (s.Length == 2)
+        {
+            return s[0] == s[1] ? 0 : 1;
+        }
+
+        return s[0] == s[s.Length - 1] ?
+               s.Substring(1, s.Length - 2).Palind() :
+               (Math.Min(
+                    s.Substring(1, s.Length - 1).Palind(),
+                    s.Substring(0, s.Length - 1).Palind()) + 1);
+    }
+
+    static void Main()
+    {
+        foreach (var x in new [] {
+                    Tuple.Create("ab", 1),
+                    Tuple.Create("aa", 0),
+                    Tuple.Create("abcd", 3),
+                    Tuple.Create("abcda", 2),
+                    Tuple.Create("abcde", 4)
+                })
+        {
+            var s = x.Item1;
+            var e = x.Item2;
+            var a = s.Palind();
+            Console.WriteLine("{0} [{1}, {2}]", s, e, a);
+        }
+    }
+}
