db1d962793d769fe8a8bbc847cc8f4bbb573b0d2
Gerula <iacobandrei@gmail.com>
2016-01-09 09:06:43 -0800

    Radix sort attempt - recursive

--- LeetCode/remote/maximum_gap.cs
+++ LeetCode/remote/maximum_gap.cs
@@ -71,6 +71,43 @@ class Solution {
         return max;
     }
 
+    public int MaxGap(int[] a)
+    {
+        var radix = Radix(a, 10, 1000000000);
+        var sorted = radix
+                     .Where(x => x != null)
+                     .SelectMany(x => x);
+        Console.WriteLine(String.Join("| ", sorted));
+        Console.WriteLine(String.Join("| ", a));
+        return sorted.Zip(sorted.Skip(1), (x, y) => y - x).Max();
+    }
+
+    public List<int>[]  Radix(int[] a, int index, int max)
+    {
+        if (index <= max)
+        {
+           a = Radix(a, index * 10, max)
+               .Where(x => x != null)
+               .SelectMany(x => x)
+               .ToArray();
+        }
+
+        return a
+               .Aggregate(
+                    new List<int>[10],
+                    (acc, x) =>
+                    {
+                        var bucket = x / index;
+                        if (acc[bucket] == null)
+                        {
+                            acc[bucket] = new List<int>();
+                        }
+
+                        acc[bucket].Add(x);
+                        return acc;
+                    });
+    }
+
     static void Main()
     {
         var random = new Random();
@@ -87,11 +124,21 @@ class Solution {
             var iffy = s.MaximumGap(vector);
             if (good != iffy)
             {
-                Console.WriteLine("For {0}. It should be {1} but it was {2}", String.Join(", ", vector), good, iffy);
+                Console.WriteLine("For {0}. It should be {1} but it was {2}.", String.Join(", ", vector), good, iffy);
             }
             else
             {
                 Console.WriteLine("For {0}. It should be {1} and it was {2}.... OK!", String.Join(", ", vector), good, iffy);
+            }
+
+            var moreIffy = s.MaxGap(vector);
+            if (good != moreIffy)
+            {
+                Console.WriteLine("For {0}. It should be {1} but it was {2}. FUCKWIT!!!", String.Join(", ", vector), good, moreIffy);
+            }
+            else
+            {
+                Console.WriteLine("For {0}. It should be {1} and it was {2}.... OK!", String.Join(", ", vector), good, moreIffy);
             }
         }
     }
