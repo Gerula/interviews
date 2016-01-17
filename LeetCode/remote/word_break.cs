0ccee79e4acf54000bc7844a78cfb7b6f87c199a
Gerula <iacobandrei@gmail.com>
2016-01-16 18:04:45 -0800

    Word break - BFS - actually DP

--- LeetCode/remote/word_break.cs
+++ LeetCode/remote/word_break.cs
@@ -14,7 +14,41 @@ using System.Collections.Generic;
 using System.Linq;
 
 public class Solution {
+    //  
+    //  Submission Details
+    //  32 / 32 test cases passed.
+    //      Status: Accepted
+    //      Runtime: 184 ms
+    //          
+    //          Submitted: 0 minutes ago
+    //
+    //  https://leetcode.com/submissions/detail/50902355/
+    //
     public bool WordBreak(string s, ISet<string> wordDict) {
+        var n = s.Length;
+        var wb = new bool[n + 1];
+        wb[0] = true;
+        for (var i = 0; i < n; i++)
+        {
+            if (!wb[i])
+            {
+                continue;
+            }
+
+            var current = s.Substring(i);
+            foreach (var word in wordDict)
+            {
+                if (current.StartsWith(word))
+                {
+                    wb[i + word.Length] = true;
+                }
+            }
+        }
+
+        return wb[n];
+    }
+
+    public bool WordBreakRec(string s, ISet<string> wordDict) {
         if (String.IsNullOrEmpty(s))
         {
             return true;
@@ -23,13 +57,15 @@ public class Solution {
         return wordDict.
                Aggregate(
                     false,
-                    (acc, x) => acc || s.StartsWith(x) && WordBreak(s.Substring(x.Length), wordDict));
+                    (acc, x) => acc || s.StartsWith(x) && WordBreakRec(s.Substring(x.Length), wordDict));
     }
 
     static void Main()
     {
         var s = new Solution();
+        Console.WriteLine(s.WordBreakRec("leetcode", new HashSet<String> { "leet", "code" }));
         Console.WriteLine(s.WordBreak("leetcode", new HashSet<String> { "leet", "code" }));
+        Console.WriteLine(s.WordBreakRec("leetcode", new HashSet<String> { "leet", "cod" }));
         Console.WriteLine(s.WordBreak("leetcode", new HashSet<String> { "leet", "cod" }));
     }
 }
