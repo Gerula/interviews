c9aa229368ab272c073a40a94166b156e14a59d9
Gerula <iacobandrei@gmail.com>
2016-01-17 16:36:15 -0800

    Repeated DNA sequences - best solution number one

--- LeetCode/remote/repeated_dns_sequences.cs
+++ LeetCode/remote/repeated_dns_sequences.cs
@@ -37,6 +37,49 @@ public class Solution {
         return set.ToList();
     }
 
+    //  
+    //  Submission Details
+    //  30 / 30 test cases passed.
+    //      Status: Accepted
+    //      Runtime: 476 ms
+    //          
+    //          Submitted: 0 minutes ago
+    //
+    //  https://leetcode.com/submissions/detail/50986443/
+    //
+    //  You are here!
+    //  Your runtime beats 100.00% of csharp submissions.
+    //
+    //  Fuckin' A!!
+    public IList<string> FindRepeatedDnaSequencesFast(string s) {
+        var set = new HashSet<String>();
+        var seen = new HashSet<long>();
+        var map = new Dictionary<char, int> { { 'A', 1 }, { 'C', 2 }, { 'G', 3 }, { 'T', 4 } };
+        long window = 0;
+        long pow = (long) Math.Pow(10, 9);
+
+        for (var i = 0; i < s.Length; i++)
+        {
+            if (i > 9)
+            {
+                window %= pow;
+            }
+
+            window = (long) (window * 10 + map[s[i]]);
+            
+            if (seen.Contains(window))
+            {
+                set.Add(s.Substring(i - 9, 10));
+            }
+            else 
+            {
+                seen.Add(window);
+            }
+        }
+
+        return set.ToList();
+    }
+
     //  Same idea but with linq. TLE
     public IList<string> FindRepeatedDnaSequences2(string s) {
         return Enumerable
@@ -51,6 +94,12 @@ public class Solution {
     static void Main()
     {
         Console.WriteLine(String.Join(Environment.NewLine, new Solution().FindRepeatedDnaSequences("AAAAACCCCCAAAAACCCCCCAAAAAGGGTTT")));
+        Console.WriteLine(String.Join(Environment.NewLine, new Solution().FindRepeatedDnaSequences("CGACGCAATTTAGAACGGGCCGCACTGCAACCATTGCTCAGACAACGCATGAGTTAAATTTCACAAGTGATAGTGGCTTGCGAGACGTGGGTTGGTGGTAGCGTACGCCCGCTATTCGCCCCTAACGTGACGGGATTATAAGGTCGCTTCCCGGAATGCGCAGACGAGTCTCCGGTTTAGCCTAGACGTCTCACGCGCGCAAGGCGTCAGTTCTCACTATCTCGCACAGGTGTATTCTATTAGTTATGGGTTCTCACTACAGTCGGTTACTTCCTCATCCATTTCTGCATACGGGTCAACTAACAGTGTCATGGGGTATTGGGAAGGATGCGTTTTTAAACCCTCTCAGTAGCGCGAGGATGCCCACAAATACGACGGCGGCCACGGATCTAATGCGAAGCTAGCGACGCTTTCCAGCAACGAGCGCCCCACTTATGACTGCGTGGGGCGCTCCGCTTTCCTAGAGAACATAGATGGTGTTTTCGAATCGTAACCACTTA")));
+        Console.WriteLine();
+        Console.WriteLine(String.Join(Environment.NewLine, new Solution().FindRepeatedDnaSequencesFast("AAAAACCCCCAAAAACCCCCCAAAAAGGGTTT")));
+        Console.WriteLine(String.Join(Environment.NewLine, new Solution().FindRepeatedDnaSequencesFast("CGACGCAATTTAGAACGGGCCGCACTGCAACCATTGCTCAGACAACGCATGAGTTAAATTTCACAAGTGATAGTGGCTTGCGAGACGTGGGTTGGTGGTAGCGTACGCCCGCTATTCGCCCCTAACGTGACGGGATTATAAGGTCGCTTCCCGGAATGCGCAGACGAGTCTCCGGTTTAGCCTAGACGTCTCACGCGCGCAAGGCGTCAGTTCTCACTATCTCGCACAGGTGTATTCTATTAGTTATGGGTTCTCACTACAGTCGGTTACTTCCTCATCCATTTCTGCATACGGGTCAACTAACAGTGTCATGGGGTATTGGGAAGGATGCGTTTTTAAACCCTCTCAGTAGCGCGAGGATGCCCACAAATACGACGGCGGCCACGGATCTAATGCGAAGCTAGCGACGCTTTCCAGCAACGAGCGCCCCACTTATGACTGCGTGGGGCGCTCCGCTTTCCTAGAGAACATAGATGGTGTTTTCGAATCGTAACCACTTA")));
+        Console.WriteLine();
         Console.WriteLine(String.Join(Environment.NewLine, new Solution().FindRepeatedDnaSequences2("AAAAACCCCCAAAAACCCCCCAAAAAGGGTTT")));
+        Console.WriteLine(String.Join(Environment.NewLine, new Solution().FindRepeatedDnaSequences2("CGACGCAATTTAGAACGGGCCGCACTGCAACCATTGCTCAGACAACGCATGAGTTAAATTTCACAAGTGATAGTGGCTTGCGAGACGTGGGTTGGTGGTAGCGTACGCCCGCTATTCGCCCCTAACGTGACGGGATTATAAGGTCGCTTCCCGGAATGCGCAGACGAGTCTCCGGTTTAGCCTAGACGTCTCACGCGCGCAAGGCGTCAGTTCTCACTATCTCGCACAGGTGTATTCTATTAGTTATGGGTTCTCACTACAGTCGGTTACTTCCTCATCCATTTCTGCATACGGGTCAACTAACAGTGTCATGGGGTATTGGGAAGGATGCGTTTTTAAACCCTCTCAGTAGCGCGAGGATGCCCACAAATACGACGGCGGCCACGGATCTAATGCGAAGCTAGCGACGCTTTCCAGCAACGAGCGCCCCACTTATGACTGCGTGGGGCGCTCCGCTTTCCTAGAGAACATAGATGGTGTTTTCGAATCGTAACCACTTA")));
     }
 }
