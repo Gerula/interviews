f2772ba9443a04ba0112c0a343086f75f58a15a0
Gerula <iacobandrei@gmail.com>
2016-01-31 13:41:08 -0800

    Triangle

---
+++ LeetCode/remote/triangle.cs
@@ -0,0 +1,37 @@
+//  https://leetcode.com/problems/triangle/
+//
+//  Given a triangle, find the minimum path sum from top to bottom. Each step you may move to adjacent numbers on the row below.
+//
+//  For example, given the following triangle
+//
+//  [
+//       [2],
+//       [3,4],
+//       [6,5,7],
+//       [4,1,8,3]
+//  ]
+//
+//  The minimum path sum from top to bottom is 11 (i.e., 2 + 3 + 5 + 1 = 11). 
+
+public class Solution {
+    //  
+    //  Submission Details
+    //  43 / 43 test cases passed.
+    //      Status: Accepted
+    //      Runtime: 164 ms
+    //          
+    //          Submitted: 0 minutes ago
+    //
+    //  https://leetcode.com/submissions/detail/52313750/
+    public int MinimumTotal(IList<IList<int>> triangle) {
+        return triangle
+               .Reverse()
+               .Skip(1)
+               .Aggregate(
+                   triangle.Last(),
+                   (acc, x) => x 
+                               .Select((val, index) => Math.Min(acc[index], acc[index + 1]) + val)
+                               .ToList())
+               .First();
+    }
+}
