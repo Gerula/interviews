b52672f89034e9ebd7590672d1ac80a843660ba2
Gerula <iacobandrei@gmail.com>
2016-02-02 21:48:07 -0800

    Summary ranges

---
+++ LeetCode/remote/summary_ranges.cs
@@ -0,0 +1,36 @@
+//  https://leetcode.com/problems/summary-ranges/
+//
+//   Given a sorted integer array without duplicates, return the summary of its ranges.
+//
+//   For example, given [0,1,2,4,5,7], return ["0->2","4->5","7"]. 
+
+public class Solution {
+    //  
+    //  Submission Details
+    //  27 / 27 test cases passed.
+    //      Status: Accepted
+    //      Runtime: 468 ms
+    //          
+    //          Submitted: 0 minutes ago
+    //
+    //  https://leetcode.com/submissions/detail/52549961/
+    //
+    public IList<string> SummaryRanges(int[] nums) {
+        return nums.Aggregate(
+                new List<Tuple<int, int>>(), 
+                (acc, x) => {
+                    if (!acc.Any() || acc.Last().Item2 != x - 1)
+                    {
+                        acc.Add(Tuple.Create(x, x));
+                    }
+                    else
+                    {
+                        acc[acc.Count - 1] = Tuple.Create(acc.Last().Item1, x);
+                    }
+                    
+                    return acc;
+                })
+                .Select(x => x.Item1 == x.Item2 ? x.Item1.ToString() : String.Format("{0}->{1}", x.Item1, x.Item2))
+                .ToList();
+    }
+}
