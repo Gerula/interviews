ac871556e6a4a55ca9119aa54c7b1c01354a28d0
Gerula <iacobandrei@gmail.com>
2016-02-04 07:32:13 -0800

    Recover tree - recursively - much slower

--- LeetCode/bootleg/recover_bst.cs
+++ LeetCode/bootleg/recover_bst.cs
@@ -74,3 +74,64 @@ public class Solution {
         b = c;
     }
 }
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    //  https://leetcode.com/submissions/detail/52672449/ 
+    //
+    //
+    //  Submission Details
+    //  1916 / 1916 test cases passed.
+    //      Status: Accepted
+    //      Runtime: 268 ms
+    //          
+    //          Submitted: 0 minutes ago
+    //
+    public void RecoverTree(TreeNode root) {
+        var pre = new TreeNode(int.MinValue);
+        TreeNode first = null;
+        TreeNode second = null;
+        Recover(root, ref pre, ref first, ref second);
+        if (first != null && second != null)
+        {
+            Swap(ref first.val, ref second.val);
+        }
+    }
+    
+    public void Recover(TreeNode root, ref TreeNode pre, ref TreeNode first, ref TreeNode second)
+    {
+        if (root == null)
+        {
+            return;
+        }
+        
+        Recover(root.left, ref pre, ref first, ref second);
+        if (root.val < pre.val)
+        {
+            if (first == null)
+            {
+                first = pre;
+            }
+            
+            second = root;
+        }
+        
+        pre = root;
+        Recover(root.right, ref pre, ref first, ref second);
+    }
+    
+    static void Swap(ref int a, ref int b)
+    {
+        var c = a;
+        a = b;
+        b = c;
+    }
+}
