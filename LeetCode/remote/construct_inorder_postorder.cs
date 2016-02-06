1b220a4f53f38ecc9c9c5b7cb8a53d34c1133677
Gerula <iacobandrei@gmail.com>
2016-02-06 09:57:00 -0800

    Improved

--- LeetCode/remote/construct_inorder_postorder.cs
+++ LeetCode/remote/construct_inorder_postorder.cs
@@ -38,3 +38,42 @@ public class Solution {
         };
     }
 }
+
+//  
+//  Submission Details
+//  202 / 202 test cases passed.
+//      Status: Accepted
+//      Runtime: 196 ms
+//          
+//          Submitted: 0 minutes ago
+//
+//  https://leetcode.com/submissions/detail/52834290/
+//
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public TreeNode BuildTree(int[] inorder, int[] postorder) {
+        return BuildTree(inorder, 0, inorder.Length - 1, postorder, 0, postorder.Length - 1);
+    }
+    
+    public TreeNode BuildTree(int[] inorder, int inorderLow, int inorderHigh, int[] postorder, int postorderLow, int postorderHigh)
+    {
+        if (inorderLow > inorderHigh)
+        {
+            return null;
+        }
+        
+        var inorderIdx = Array.IndexOf(inorder, postorder[postorderHigh]);
+        return new TreeNode(postorder[postorderHigh]) {
+            left = BuildTree(inorder, inorderLow, inorderIdx - 1, postorder, postorderLow, postorderLow + inorderIdx - inorderLow - 1),
+            right = BuildTree(inorder, inorderIdx + 1, inorderHigh, postorder, postorderLow + inorderIdx - inorderLow, postorderHigh - 1)
+        };
+    }
+}
