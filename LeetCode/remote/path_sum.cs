37f62dab6de6e730338d3775e9b396150727ed0e
Gerula <iacobandrei@gmail.com>
2016-02-08 17:28:57 -0800

    Path sum

---
+++ LeetCode/remote/path_sum.cs
@@ -0,0 +1,39 @@
+//  https://leetcode.com/problems/path-sum/
+//
+//   Given a binary tree and a sum, determine if the tree has a root-to-leaf path such that adding up all the values along the path equals the given sum. 
+
+//  
+//  Submission Details
+//  114 / 114 test cases passed.
+//      Status: Accepted
+//      Runtime: 168 ms
+//          
+//          Submitted: 0 minutes ago
+//
+//  https://leetcode.com/submissions/detail/52996618/
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public bool HasPathSum(TreeNode root, int sum) {
+        if (root == null)
+        {
+            return false;
+        }
+        
+        if (root.left == root.right && root.right == null)
+        {
+            return sum == root.val;
+        }
+        
+        return HasPathSum(root.left, sum - root.val) ||
+               HasPathSum(root.right, sum - root.val);
+    }
+}
