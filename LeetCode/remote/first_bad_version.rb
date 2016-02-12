82054d92ea81c54b1b7219e968b698dfde43a036
Gerula <iacobandrei@gmail.com>
2016-02-11 20:27:25 -0800

    First bad version

---
+++ LeetCode/remote/first_bad_version.rb
@@ -0,0 +1,30 @@
+#   https://leetcode.com/problems/first-bad-version/
+#
+#   You are a product manager and currently leading a team to develop a new product.
+#   Unfortunately, the latest version of your product fails the quality check.
+#   Since each version is developed based on the previous version, all the versions after a bad version are also bad. 
+
+#   
+#   Submission Details
+#   21 / 21 test cases passed.
+#       Status: Accepted
+#       Runtime: 64 ms
+#           
+#           Submitted: 0 minutes ago
+#
+#   https://leetcode.com/submissions/detail/53263348/
+
+def first_bad_version(n)
+    low = 1
+    high = n
+    while low <= high
+        mid = low + (high - low) / 2
+        if is_bad_version(mid)
+            high = mid - 1
+        else
+            low = mid + 1
+        end
+    end
+    
+    return low
+end
