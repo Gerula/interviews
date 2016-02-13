d0cca126b98a20dd34089abb0aad760fa6407d25
Gerula <iacobandrei@gmail.com>
2016-02-12 21:14:55 -0800

    This fucking OJ is aleasy tripping when Ruby meets pseudopolynomial dp

--- LeetCode/remote/coin_chanage.rb
+++ LeetCode/remote/coin_chanage.rb
@@ -10,3 +10,13 @@ def coin_change(coins, amount)
     return coins.size if amount < 0
     return coins.map { |x| coin_change(coins, amount - x) + 1 }.min
 end
+
+#   TLE Again. But this is the fucking solution
+def coin_change(coins, amount)
+    dp = Array.new(amount) { 0 }
+    for i in 1..amount
+        dp[i] = coins.select{ |x| x <= i }.map { |x| dp[i - x] + 1}.min || amount + 1
+    end
+    
+    dp[amount].nil? || dp[amount] > amount ? -1 : dp[amount]
+end
