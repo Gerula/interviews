7e9538be4a724c06d3e960f3199d79b7d3c95e4d
Gerula <iacobandrei@gmail.com>
2016-02-14 21:41:54 -0800

    Sorted list to BST

---
+++ LeetCode/remote/sorted_list_to_bst_two.cs
@@ -0,0 +1,66 @@
+//  https://leetcode.com/problems/convert-sorted-list-to-binary-search-tree/
+//
+//  Given a singly linked list where elements are sorted in ascending order, convert it to a height balanced BST.
+//
+//  Nice one. Did it all in the browser and it got accepted pretty good altough it's the shitty naive version.
+//
+//  https://leetcode.com/submissions/detail/53503625/
+//
+//
+//  Submission Details
+//  32 / 32 test cases passed.
+//      Status: Accepted
+//      Runtime: 168 ms
+//          
+//          Submitted: 0 minutes ago
+//
+//  You are here!
+//  Your runtime beats 60.00% of csharp submissions.
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public int val;
+ *     public ListNode next;
+ *     public ListNode(int x) { val = x; }
+ * }
+ */
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public TreeNode SortedListToBST(ListNode head) {
+        if (head == null)
+        {
+            return null;
+        }
+        
+        if (head.next == null)
+        {
+            return new TreeNode(head.val);    
+        }
+        
+        var slow = head;
+        var pre = slow;
+        var fast = head.next;
+        while (fast != null && fast.next != null)
+        {
+            pre = slow;
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        
+        var mid = pre.next;
+        pre.next = null;
+        var result = new TreeNode(mid.val);
+        result.left = SortedListToBST(head);
+        result.right = SortedListToBST(mid.next);
+        return result;
+    }
+}
