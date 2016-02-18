551bfa2a1f35f4f3d83fde40f8df980435a865c1
Gerula <iacobandrei@gmail.com>
2016-02-17 20:26:22 -0800

    Unique BSTs - Poor DP

--- LeetCode/remote/unique_binary_search_trees.rb
+++ LeetCode/remote/unique_binary_search_trees.rb
@@ -10,6 +10,17 @@ def num_trees(n)
     trees(1, n)
 end
 
-def trees(low, high)
-    low >= high ? 1 : low.upto(high).map { |x| trees(low, x - 1) * trees(x + 1, high) }.inject(:+)
+#   https://leetcode.com/submissions/detail/53789799/
+#   
+#   Submission Details
+#   19 / 19 test cases passed.
+#       Status: Accepted
+#       Runtime: 84 ms
+#           
+#           Submitted: 0 minutes ago
+#
+#   Pulled off a poor man's DP. It's so beautiful it hurts.
+def trees(low, high, cache)
+    return 1 if low >= high
+    cache["#{low}-#{high}"] ||= low.upto(high).map { |x| trees(low, x - 1, cache) * trees(x + 1, high, cache) }.inject(:+)
 end
