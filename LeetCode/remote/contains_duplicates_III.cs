da7b8ac1f8048ab8aab0898d6948e73f0a84d55e
Gerula <iacobandrei@gmail.com>
2016-02-17 17:40:39 -0800

    Contains dupes III - stupid confusing problem

--- LeetCode/remote/contains_duplicates_III.cs
+++ LeetCode/remote/contains_duplicates_III.cs
@@ -9,6 +9,78 @@ using System.Collections.Generic;
 using System.Linq;
 
 public class Solution {
+    class Dequeue
+    {
+        private LinkedList<int> storage = new LinkedList<int>();
+        
+        public Dequeue()
+        {
+            Empty = true;
+        }
+        
+        public void Add(int x, Func<int, int, bool> comparer)
+        {
+            while (storage.Count > 0 && comparer(storage.Last(), x))
+            {
+                storage.RemoveLast();
+            }
+            
+            storage.AddLast(x);
+            Empty = false;
+        }
+        
+        public void Remove(int x)
+        {
+            if (storage.Count > 0 && storage.First() == x)
+            {
+                storage.RemoveFirst();
+            }
+            
+            Empty = storage.Count == 0;
+        }
+        
+        public int Head {
+            get {
+                return storage.First();
+            }
+        }
+        
+        public bool Empty { 
+            get; 
+            private set; 
+        }
+    }
+    
+    public bool ContainsNearbyAlmostDuplicate(int[] nums, int k, int t) {
+        var max = new Dequeue();
+        var min = new Dequeue();
+        for (var i = 0; i < Math.Min(k, nums.Length); i++)
+        {
+            max.Add(nums[i], (a, b) => a >= b);
+            min.Add(nums[i], (a, b) => a < b);
+        }
+
+        if (!max.Empty && !min.Empty && max.Head - min.Head <= t)
+        {
+            return true;
+        }
+        
+        for (var i = k; i < nums.Length; i++)
+        {
+            max.Add(nums[i], (a, b) => a >= b);
+            min.Add(nums[i], (a, b) => a < b);
+            max.Remove(nums[i - k]);
+            min.Remove(nums[i - k]);
+            
+            if (!max.Empty && !min.Empty && max.Head - min.Head <= t)
+            {
+                return true;
+            }
+        }
+        
+        return false;
+    }
+
     public bool ContainsNearbyAlmostDuplicate(int[] nums, int k, int t) {
         var set = new SortedDictionary<int, int>();
 
