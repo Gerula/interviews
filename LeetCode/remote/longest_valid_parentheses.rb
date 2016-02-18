ea1fbfc77a07fce4b854f94d9e1f13b81e7962a6
Gerula <iacobandrei@gmail.com>
2016-02-17 21:11:22 -0800

    Smart

--- LeetCode/remote/longest_valid_parentheses.rb
+++ LeetCode/remote/longest_valid_parentheses.rb
@@ -11,15 +11,25 @@
 
 # @param {String} s
 # @return {Integer}
+#  https://leetcode.com/submissions/detail/53794023/
+#
+#  Submission Details
+#  229 / 229 test cases passed.
+#   Status: Accepted
+#   Runtime: 144 ms
+#       
+#       Submitted: 0 minutes ago
+# No matter what they say, this is a hard fucking problem
 def longest_valid_parentheses(s)
     dp = Array.new(s.size) { 0 }
     max = 0
     for i in 1...s.size
         next if s[i] == '('
         dp[i] = 2 + (dp[i - 2] || 0) if s[i - 1] == '('
-        dp[i] = 2 + dp[i - dp[i - 1]] if s[i - 1] == ')' && i - dp[i - 1] >= 0
+        dp[i] = 2 + dp[i - 1] + (dp[i - dp[i - 1] - 2] || 0) if s[i - 1] == ')' && 
+                                                                i - dp[i - 1] - 1 >= 0 && 
+                                                                s[i - dp[i - 1] - 1] == '('
         max = [max, dp[i]].max
-    end
-    
+    end 
     return max
 end
