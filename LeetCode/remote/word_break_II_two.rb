8afafb6704c0bd63a49ea202a080e3a196dfbf59
Gerula <iacobandrei@gmail.com>
2016-02-25 17:21:13 -0800

    Word break II

--- LeetCode/remote/word_break_II_two.rb
+++ LeetCode/remote/word_break_II_two.rb
@@ -20,3 +20,26 @@ def word_break(s, word_dict)
     }
     .flatten
 end
+
+#   Poor man's DP
+#   https://leetcode.com/submissions/detail/54548195/
+#
+#   Submission Details
+#   37 / 37 test cases passed.
+#       Status: Accepted
+#       Runtime: 124 ms
+#           
+#           Submitted: 0 minutes ago
+#
+def word_break(s, word_dict, hash = nil)
+    return [""] if s.to_s.empty?
+    hash ||= {}
+    return hash[s] if !hash[s].nil?
+    hash[s] = word_dict
+              .select { |x| s.start_with?(x) }
+              .map { |x| 
+                     word_break(s[x.size..-1], word_dict, hash)
+                     .map { |y| "#{x} #{y}".strip }
+              }
+              .flatten
+end
