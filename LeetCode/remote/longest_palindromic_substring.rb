30941e1ff6727d02e0ff107e846220d1458ef677
Gerula <iacobandrei@gmail.com>
2016-02-26 20:44:00 -0800

    TLE Longest palindromic - DP

--- LeetCode/remote/longest_palindromic_substring.rb
+++ LeetCode/remote/longest_palindromic_substring.rb
@@ -28,3 +28,31 @@ def palindrome(s, low, high)
 
     s[low + 1..high - 1]
 end
+
+#   DP TLE
+def longest_palindrome(s)
+    return s if s.to_s.nil?
+    palind = Array.new(s.size) {
+        Array.new(s.size)
+    }  
+    
+    0.upto(s.size - 1).each { |i|
+        palind[i][i] = true
+    }
+    
+    max = s[0]
+    0.upto(s.size - 1).each { |i|
+        2.upto(s.size - i) { |j|
+            if j == i + 1
+                palind[i][i + 1] = s[i] == s[i + 1]
+            else
+                palind[i][j] = s[i] == s[j] && palind[i + 1][j - 1]
+            end
+            
+            max = s[i, j] if palind[i][j] && s[i, j].size > max.size
+        }
+    }
+    
+    max
+end
+
