095456635eadac802f38d7220a0f4bc2a9e49909
Gerula <iacobandrei@gmail.com>
2016-02-28 11:14:06 -0800

    Quicksort instead of insertion - still TLE - fuckers..

--- LeetCode/remote/insertion_sort_list.rb
+++ LeetCode/remote/insertion_sort_list.rb
@@ -18,3 +18,44 @@ def insertion_sort_list(head)
     
     dummy.next
 end
+
+# Clearly tripping balls. This is quicksort and still TLE
+
+def insertion_sort_list(head)
+    return head if head.nil? || head.next.nil?
+    lower = ListNode.new(:marker)
+    equal = ListNode.new(:marker)
+    larger = ListNode.new(:marker)
+    lower_end = lower
+    equal_end = equal
+    larger_end = larger
+    pivot = head
+    it = head
+    while it
+        next_it = it.next
+        it.next = nil
+        if it.val < pivot.val
+            lower_end.next = it
+            lower_end = lower_end.next
+        elsif it.val > pivot.val
+            larger_end.next = it
+            larger_end = larger_end.next
+        else
+            equal_end.next = it
+            equal_end = equal_end.next
+        end
+        
+        it = next_it
+    end
+    
+    lower.next = insertion_sort_list(lower.next)
+    larger.next = insertion_sort_list(larger.next)
+    it = lower
+    while it && it.next
+        it = it.next
+    end
+    
+    it.next = equal.next
+    equal_end.next = larger.next
+    return lower.next
+end
