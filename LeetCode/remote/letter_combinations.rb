b1a7be07a347da039e12f1f6e3824df7efb81ed9
Gerula <iacobandrei@gmail.com>
2016-02-29 06:55:54 -0800

    Letter combinations from phone number

---
+++ LeetCode/remote/letter_combinations.rb
@@ -0,0 +1,25 @@
+#   https://leetcode.com/problems/letter-combinations-of-a-phone-number/
+#
+#   Given a digit string, return all possible letter combinations that the number could represent. 
+#   https://leetcode.com/submissions/detail/54898669/
+#
+#   Submission Details
+#   25 / 25 test cases passed.
+#       Status: Accepted
+#       Runtime: 76 ms
+#           
+#           Submitted: 0 minutes ago
+
+@nums = { 1 => "", 2 => "abc", 3 => "def", 4 => "ghi", 5 => "jkl",
+          6 => "mno", 7 => "pqrs", 8 => "tuv", 9 => "wxyz", 0 => "" }
+          
+def letter_combinations(digits)
+    return [] if digits.to_s.empty?
+    return @nums[digits.to_i].chars if digits.size == 1
+    letter_combinations(digits[1..-1]).map { |right|
+        @nums[digits.chars.first.to_i].chars.map { |digit|
+            digit + right
+        }
+    }
+    .flatten
+end
