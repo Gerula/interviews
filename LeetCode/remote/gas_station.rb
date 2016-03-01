240248982b66f2b47fded24ccf8d4c0bb6a2e9c7
Gerula <iacobandrei@gmail.com>
2016-02-29 21:44:02 -0800

    Gas station

--- LeetCode/remote/gas_station.rb
+++ LeetCode/remote/gas_station.rb
@@ -16,3 +16,28 @@ def can_complete_circuit(gas, cost)
     carry += gas[gas.size - 1] - cost[gas.size - 1]
     return carry >= 0 ? status.first : -1
 end
+
+#   https://leetcode.com/submissions/detail/54966024/
+#   
+#   Submission Details
+#   16 / 16 test cases passed.
+#       Status: Accepted
+#   Runtime: 96 ms
+#    
+#   Submitted: 1 minute ago
+
+def can_complete_circuit(gas, cost)
+    total = 0
+    trip = 0
+    start = 0
+    (0...gas.size).each { |i|
+        trip += gas[i] - cost[i]
+        total += trip
+        if trip < 0
+            trip = 0
+            start = i + 1
+        end
+    }
+    
+    total < 0 ? - 1 : start
+end
