f94675a7d6f2f0778d2e5b5a8008bc4bfac9d670
Gerula <iacobandrei@gmail.com>
2016-02-29 17:54:50 -0800

    Search index position

---
+++ LeetCode/remote/search_index_position.rb
@@ -0,0 +1,22 @@
+#   https://leetcode.com/problems/search-insert-position/
+#   Given a sorted array and a target value, return the index if the target is found.
+#   If not, return the index where it would be if it were inserted in order.
+#
+#   You may assume no duplicates in the array.
+#   https://leetcode.com/submissions/detail/54941335/
+
+def search_insert(nums, target)
+    low = 0
+    high = nums.size
+    while low < high
+        mid = low + (high - low) / 2
+        return mid if nums[mid] == target
+        if nums[mid] < target
+            low = mid + 1
+        else
+            high = mid
+        end
+    end
+    
+    return low
+end
