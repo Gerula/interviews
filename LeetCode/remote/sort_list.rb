8b67b8cea4590f55be61b5b9109a65c5738e919b
Gerula <iacobandrei@gmail.com>
2016-03-04 20:57:00 -0800

    Sort list

--- LeetCode/remote/sort_list.rb
+++ LeetCode/remote/sort_list.rb
@@ -67,3 +67,38 @@ def sort_list(head)
     it.next = larger.next
     return smaller.next
 end
+
+#   https://leetcode.com/submissions/detail/55374749/
+#
+#   Submission Details
+#   15 / 15 test cases passed.
+#       Status: Accepted
+#       Runtime: 320 ms
+#           
+#           Submitted: 0 minutes ago
+def sort_list(head)
+    return head if head.nil? || head.next.nil?
+    half = head
+    finish = head.next
+    while finish && finish.next
+        half = half.next
+        finish = finish.next.next
+    end
+    
+    mid = half.next
+    half.next = nil
+    return merge(sort_list(head), sort_list(mid))
+end
+
+def merge(a, b)
+    return nil if a.nil? && b.nil?
+    return a if b.nil?
+    return b if a.nil?
+    if a.val < b.val
+        a.next = merge(a.next, b)
+        return a
+    end
+    
+    b.next = merge(a, b.next)
+    return b
+end
