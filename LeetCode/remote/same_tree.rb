df580d8b81a9eaef8a82f1f951f3b9acbe82762b
Gerula <iacobandrei@gmail.com>
2016-03-07 17:47:40 -0800

    Same tree II

--- LeetCode/remote/same_tree.rb
+++ LeetCode/remote/same_tree.rb
@@ -16,3 +16,21 @@ def is_same_tree(p, q)
            is_same_tree(p.left, q.left) &&
            is_same_tree(p.right, q.right)
 end
+
+#   https://leetcode.com/submissions/detail/55657432/
+#
+#   Submission Details
+#   54 / 54 test cases passed.
+#       Status: Accepted
+#       Runtime: 72 ms
+#           
+#           Submitted: 0 minutes ago
+class TreeNode
+    def hash
+        "#{self.val},#{self.left.nil? ? "." : self.left.hash},#{self.right.nil? ? "." : self.right.hash}"
+    end
+end
+
+def is_same_tree(p, q)
+    return p.hash == q.hash
+end
