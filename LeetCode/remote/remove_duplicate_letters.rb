98a54ba7d457dd2b92b0a39a9aa76ca53e8b2b62
Gerula <iacobandrei@gmail.com>
2016-03-08 20:16:14 -0800

    Remove duplicate letters

--- LeetCode/remote/remove_duplicate_letters.rb
+++ LeetCode/remote/remove_duplicate_letters.rb
@@ -21,3 +21,25 @@ def remove_duplicate_letters(s)
     
     s[start] + remove_duplicate_letters((s[start + 1..-1] || "").gsub(s[start], ""))
 end
+
+#   https://leetcode.com/submissions/detail/55780403/
+def remove_duplicate_letters(s)
+    return "" if s.to_s.empty?
+    counts = s.chars.reduce({}) { |acc, x|
+        acc[x] ||= 0
+        acc[x] += 1
+        acc
+    }
+    
+    idx = 0
+    (0...s.size).each { |i|
+        if s[i] < s[idx]
+            idx = i
+        end
+        
+        counts[s[i]] -= 1
+        break if counts[s[i]] == 0
+    }
+    
+    s[idx] + remove_duplicate_letters(s[idx  + 1..-1].gsub(s[idx], ""))
+end
