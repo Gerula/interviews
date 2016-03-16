3c4d4b298ab23b7af6ce4108a7cf1a9c429bcf89
Gerula <iacobandrei@gmail.com>
2016-03-15 21:08:09 -0700

    Add and search word datastructure - trie

---
+++ LeetCode/remote/add_and_search_word_datastructure_design.rb
@@ -0,0 +1,36 @@
+#   https://leetcode.com/problems/add-and-search-word-data-structure-design/
+#    Design a data structure that supports the following two operations:
+#
+#    void addWord(word)
+#    bool search(word)
+#
+#    search(word) can search a literal word or a regular expression string containing only letters a-z or .. A . means it can represent any one letter.
+#
+#   https://leetcode.com/submissions/detail/56424028/
+#
+#   Submission Details
+#   13 / 13 test cases passed.
+#       Status: Accepted
+#       Runtime: 1744 ms
+#           
+#           Submitted: 1 minute ago
+class WordDictionary
+    def add_word(word)
+        @trie ||= {}
+        word.chars.reduce(@trie) { |acc, x|
+            acc[x] ||= {}
+            acc[x]
+        }[:end] = true
+    end
+
+    def search(word, trie = {})
+        trie = @trie if trie == {}
+        return false if trie.nil?
+        return !trie[:end].nil? if word.to_s.empty?
+        return (trie.keys - [:end]).reduce(false) { |acc, x|
+            acc || search(word[1..-1], trie[x])
+        } if word[0] == "."
+        return false if trie[word[0]].nil?
+        return search(word[1..-1], trie[word[0]])
+    end
+end
