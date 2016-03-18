de0fdd3b4175fa63ad4d93a54e95e4ad6529209f
Gerula <iacobandrei@gmail.com>
2016-03-18 06:53:29 -0700

    Counting bits

---
+++ LeetCode/remote/counting_bits.rb
@@ -0,0 +1,22 @@
+#   https://leetcode.com/problems/counting-bits/
+#   Given a non negative integer number num.
+#   For every numbers i in the range 0 ≤ i ≤ num calculate the number of 1's in their binary representation and return them as an array. 
+#   https://leetcode.com/submissions/detail/56651730/
+#
+#   Submission Details
+#   16 / 16 test cases passed.
+#       Status: Accepted
+#       Runtime: 624 ms
+#           
+#           Submitted: 0 minutes ago
+#   Basically Gray Code idea.
+# @param {Integer} num
+# @return {Integer[]}
+def count_bits(num)
+    counts = [0]
+    while counts.length < num + 1
+        counts += counts.map { |x| x + 1 }
+    end
+
+    counts[0..num]
+end
