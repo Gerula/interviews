7bd665bae68b8a7623a7cfc41c947e5402a63f88
Gerula <iacobandrei@gmail.com>
2016-03-20 17:39:53 -0700

    Merge k lists - nice attempt but doesn't handle nil right

---
+++ LeetCode/remote/merge_k_lists.rb
@@ -0,0 +1,33 @@
+#   https://leetcode.com/problems/merge-k-sorted-lists/
+#   Merge k sorted linked lists and return it as one sorted list. Analyze and describe its complexity. 
+# Definition for singly-linked list.
+# class ListNode
+#     attr_accessor :val, :next
+#     def initialize(val)
+#         @val = val
+#         @next = nil
+#     end
+# end
+
+class ListNode
+    def +(other)
+        return merge(self, other) 
+    end
+end
+
+def merge_k_lists(lists)
+    lists.reduce(:+)
+end
+
+def merge(a, b)
+    return a if b.nil?
+    return b if a.nil?
+    if (a.val < b.val)
+        a.next = merge(a.next, b)
+        return a
+    end
+
+    b.next = merge(a, b.next)
+    return b
+end
+
