ea28cd4307667802f88d055a71dd832db86b1d5d
Gerula <iacobandrei@gmail.com>
2016-03-21 18:27:39 -0700

    First bad version

--- LeetCode/remote/first_bad_version.rb
+++ LeetCode/remote/first_bad_version.rb
@@ -28,3 +28,17 @@ def first_bad_version(n)
     
     return low
 end
+
+def first_bad_version(n)
+    low, high = 1, n
+    while low < high
+        mid = low + ((high - low) >> 2)
+        if is_bad_version(mid)
+           high = mid 
+       else
+           low = mid + 1
+       end
+    end
+    
+    low
+end
