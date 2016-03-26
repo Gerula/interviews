65bad0fbc9373ae63b6a5482c578324c6ccd3ac2
Gerula <iacobandrei@gmail.com>
2016-03-26 13:14:53 -0700

    Shortest palindrome - smart

--- LeetCode/remote/shortest_palindrome.rb
+++ LeetCode/remote/shortest_palindrome.rb
@@ -5,6 +5,7 @@
 require 'test/unit'
 extend Test::Unit::Assertions
 
+# Memory limit exceeded
 def shortest_palindrome(s)
     idx = 0
     (1..s.size).each { |i|
@@ -14,5 +15,29 @@ def shortest_palindrome(s)
     s[idx..-1].reverse + s
 end
 
+#   https://leetcode.com/submissions/detail/57364432/
+#
+#   Submission Details
+#   117 / 117 test cases passed.
+#       Status: Accepted
+#       Runtime: 716 ms
+#           
+#           Submitted: 0 minutes ago
+#
+def shortest_palindrome_2(s)
+    idx = 0
+    front = ""
+    back = ""
+    (0...s.size).each { |i|
+        front = front + s[i]
+        back = s[i] + back
+        idx = [idx, i].max if front == back
+    }
+
+    (s[idx + 1..-1] || "").reverse + s
+end
+
+assert("aaacecaaa" == shortest_palindrome_2("aacecaaa"))
+assert("dcbabcd" == shortest_palindrome_2("abcd"))
 assert("aaacecaaa" == shortest_palindrome("aacecaaa"))
 assert("dcbabcd" == shortest_palindrome("abcd"))
