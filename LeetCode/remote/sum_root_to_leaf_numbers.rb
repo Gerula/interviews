082ba775c63e579e46719c3ea28e8a4a94e2e04e
Gerula <iacobandrei@gmail.com>
2016-03-27 12:05:13 -0700

    Sum root to leaf numbers

--- LeetCode/remote/sum_root_to_leaf_numbers.rb
+++ LeetCode/remote/sum_root_to_leaf_numbers.rb
@@ -15,3 +15,21 @@ def sum_numbers(root, parent = 0)
     return parent * 10 + root.val if root.left.nil? && root.right.nil?
     return sum_numbers(root.left, parent * 10 + root.val) + sum_numbers(root.right, parent * 10 + root.val)
 end
+
+# LoL @ Coding olympics
+# Exact same fucking thing
+# https://leetcode.com/submissions/detail/57444479/
+#
+# Submission Details
+# 109 / 109 test cases passed.
+#   Status: Accepted
+#   Runtime: 88 ms
+#       
+#       Submitted: 0 minutes ago
+# You are here!
+# Your runtime beats 100.00% of rubysubmissions.
+def sum_numbers(root, parent = 0)
+    return 0 if root.nil?
+    return parent * 10 + root.val if root.left.nil? && root.right.nil?
+    return sum_numbers(root.left, parent * 10 + root.val) + sum_numbers(root.right, parent * 10 + root.val)
+end
