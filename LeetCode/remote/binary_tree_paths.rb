d623532bb8a5018a8d64deb78f661df4d829640a
Gerula <iacobandrei@gmail.com>
2016-03-28 06:41:03 -0700

    Binary tree paths

--- LeetCode/remote/binary_tree_paths.rb
+++ LeetCode/remote/binary_tree_paths.rb
@@ -9,3 +9,21 @@ def binary_tree_paths(root)
         "#{root.val}->#{x}"
     }
 end
+
+#   https://leetcode.com/submissions/detail/57522804/
+#
+#   Submission Details
+#   209 / 209 test cases passed.
+#       Status: Accepted
+#       Runtime: 72 ms
+#           
+#           Submitted: 0 minutes ago
+#
+def binary_tree_paths(root)
+    return [] if root.nil?
+    return [root.val.to_s] if root.left.nil? && root.right.nil?
+    (binary_tree_paths(root.left) +
+    binary_tree_paths(root.right)).map { |x|
+        "#{root.val}->#{x}"
+    }
+end
