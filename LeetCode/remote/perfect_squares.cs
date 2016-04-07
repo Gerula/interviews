ab422f2aac0d3275bf5d3ebf4ef6d5c9d1434dc1
Gerula <iacobandrei@gmail.com>
2016-04-07 06:18:05 -0700

    Perfect squares

--- LeetCode/remote/perfect_squares.cs
+++ LeetCode/remote/perfect_squares.cs
@@ -41,3 +41,72 @@ public class Solution {
         Console.WriteLine(s.NumSquares(1535));
     }
 }
+
+// It's evolution, baby!
+// TLE:
+public class Solution {
+    public int NumSquares(int n) {
+        if (n < 2)
+        {
+            return n;
+        }
+        
+        return Enumerable
+               .Range(1, (int) Math.Sqrt(n))
+               .Select(x => 1 + NumSquares(n - x * x))
+               .Min();
+    }
+}
+
+//  Output limit exceeded
+public class Solution {
+    public int NumSquares(int n, Dictionary<int, int> hash = null) {
+        if (hash == null)
+        {
+            hash = new Dictionary<int, int>();
+        }
+        
+        if (n < 2)
+        {
+            return n;
+        }
+        
+        if (hash.ContainsKey(n))
+        {
+            return hash[n];
+        }
+        
+        hash[n] = Enumerable
+                  .Range(1, (int) Math.Sqrt(n))
+                  .Select(x => 1 + NumSquares(n - x * x, hash))
+                  .Min();
+        return hash[n];
+    }
+}
+
+//  https://leetcode.com/submissions/detail/58410276/
+//
+//  Submission Details
+//  600 / 600 test cases passed.
+//      Status: Accepted
+//      Runtime: 1100 ms
+//          
+//          Submitted: 1 minute ago
+//
+public class Solution {
+    public int NumSquares(int n, Dictionary<int, int> hash = null) {
+        var dp = new int[n + 1];
+        dp[0] = 0;
+        dp[1] = 1;
+        for (var i = 2; i <= n; i++)
+        {
+            dp[i] = Enumerable
+                    .Range(1, (int) Math.Sqrt(i))
+                    .Select(x => 1 + dp[i - x * x])
+                    .Min();
+        }
+        
+        return dp[n];
+    }
+}
+    
