bad95f7b71a4957ae2797f525e5d192548ff0990
Gerula <iacobandrei@gmail.com>
2016-04-11 20:24:01 -0700

    Max product of word lengths

---
+++ LeetCode/remote/maximum_product_of_word_lengths.cs
@@ -0,0 +1,37 @@
+//  https://leetcode.com/problems/maximum-product-of-word-lengths/
+//  Given a string array words, find the maximum value of length(word[i]) * length(word[j])
+//  where the two words do not share common letters.
+//  You may assume that each word will contain only lower case letters.
+//  If no such two words exist, return 0. 
+//  https://leetcode.com/submissions/detail/58798912/
+public class Solution {
+    public int MaxProduct(string[] words) {
+        var hash = words.Aggregate(
+            new Dictionary<String, long>(),
+            (acc, x) => {
+                if (!acc.ContainsKey(x))
+                {
+                    acc[x] = x.Aggregate((long) 0, (a, b) => { 
+                        return a | (1 << (b - 'a'));
+                    });
+                }
+                    
+                return acc;
+            });
+        
+        var max = 0;
+        for (var i = 1; i < words.Length; i++)
+        {
+            for (var j = 0; j < i; j++)
+            {
+                if ((hash[words[i]] & hash[words[j]]) == 0)
+                {
+                    max = Math.Max(max, words[i].Length * words[j].Length);
+                }
+            }
+        }
+        
+        return max;
+    }
+}
+
