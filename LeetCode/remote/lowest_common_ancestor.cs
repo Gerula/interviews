3ce03dd33907bb276d0a4f6f0bb557c5d49a64a6
Gerula <iacobandrei@gmail.com>
2016-05-03 07:28:37 -0700

    LCA

--- LeetCode/remote/lowest_common_ancestor.cs
+++ LeetCode/remote/lowest_common_ancestor.cs
@@ -24,4 +24,31 @@ public class Solution {
         
         return left == null ? right : left;
     }
+
+//  Same fucking thing
+//  https://leetcode.com/submissions/detail/60617537/
+//
+//  Submission Details
+//  31 / 31 test cases passed.
+//      Status: Accepted
+//      Runtime: 168 ms
+//          
+//          Submitted: 0 minutes ago
+//  You are here!
+//  Your runtime beats 97.26% of csharpsubmissions.
+    public TreeNode LowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == null || root == p || root == q)
+        {
+            return root;
+        }
+        
+        var left = LowestCommonAncestor(root.left, p, q);
+        var right = LowestCommonAncestor(root.right, p, q);
+        if (left != null && right != null)
+        {
+            return root;
+        }
+        
+        return left == null ? right : left;
+    }
 }
