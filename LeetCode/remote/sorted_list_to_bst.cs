fe3e1d9ddf8359350ae7f0598700e382f81e8800
Gerula <iacobandrei@gmail.com>
2016-05-04 06:18:54 -0700

    Sorted list to bst

--- LeetCode/remote/sorted_list_to_bst.cs
+++ LeetCode/remote/sorted_list_to_bst.cs
@@ -1,5 +1,4 @@
 // https://leetcode.com/submissions/detail/33889944/
-
 /**
  * Definition for singly-linked list.
  * public class ListNode {
@@ -36,6 +35,46 @@ public class Solution {
         return new TreeNode(value) { left = left, right = right };
    } 
     
+   //   https://leetcode.com/submissions/detail/60699959/
+   //
+   //   Submission Details
+   //   32 / 32 test cases passed.
+   //       Status: Accepted
+   //       Runtime: 172 ms
+   //           
+   //           Submitted: 1 minute ago
+   //
+   public TreeNode SortedListToBST(ListNode head) 
+   {
+        if (head == null)
+        {
+            return null;
+        }
+        
+        if (head.next == null)
+        {
+            return new TreeNode(head.val);
+        }
+        
+        var pre = head;
+        var slow = head;
+        var fast = head.next;
+        while (fast != null && fast.next != null)
+        {
+            pre = slow;
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        
+        var first = head;
+        var second = pre.next;
+        pre.next = null;
+        return new TreeNode(second.val) {
+            left = SortedListToBST(first),
+            right = SortedListToBST(second.next)
+        };
+    }
+
     public int GetLength(ListNode head) {
         int count = 0;
         for (; head != null; count++, head = head.next) {}
