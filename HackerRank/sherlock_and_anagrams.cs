07bcac6834bacdc3a47ef50ea4aa2ead2f729678
Gerula <iacobandrei@gmail.com>
2016-05-17 19:46:21 -0700

    Passed

--- HackerRank/sherlock_and_anagrams.cs
+++ HackerRank/sherlock_and_anagrams.cs
@@ -8,25 +8,42 @@ class Solution {
     static int NumPairs(String s) {
         var count = 0;
         for (var i = 1; i < s.Length; i++) {
-            var pairs = new List<String>();
+            var hash = new Dictionary<String, int>();
             for (var j = 0; j < s.Length - i + 1; j++) {
-                pairs.Add(s.Substring(j, i));
+                var substring = Hash(s.Substring(j, i));
+                if (!hash.ContainsKey(substring)) {
+                    hash[substring] = 0;
+                }
+                
+                hash[substring]++;
             }
             
-            for (var first = 1; first < pairs.Count; first++) {
-                for (var second = 0; second < first; second++) {
-                    if (IsAnagram(pairs[first], pairs[second])) {
-                        count++;
-                    }
-                }
-            }
+            count += hash.Keys.Select(x => hash[x] < 2 ? 0 : hash[x] * (hash[x] - 1) / 2).Sum();
         }
         
         return count;
     }
     
-    static bool IsAnagram(String a, String b) {
-        return a.OrderBy(x => x).SequenceEqual(b.OrderBy(x => x));
+    static String Hash(String s) {
+        var hash = s.Aggregate(
+            new Dictionary<char, int>(),
+            (acc, x) => {
+                if (!acc.ContainsKey(x)) {
+                    acc[x] = 0;
+                } 
+                
+                acc[x]++;
+                return acc;
+            });
+        
+        return String
+               .Join(
+                  String.Empty,
+                  Enumerable.Range('a', 'z' - 'a' + 1)
+                  .Select(
+                      x => hash.ContainsKey((char) x) ? 
+                      String.Format("{0}:{1}", (char) x, hash[(char) x]) :
+                      String.Empty));
     }
     
     /* 
