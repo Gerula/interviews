8c315ae47c05c7a7a0c72f2514f83af1756f6b98
Gerula <iacobandrei@gmail.com>
2016-05-30 10:49:57 -0700

    maximal square

--- LeetCode/remote/maximal_square.cs
+++ LeetCode/remote/maximal_square.cs
@@ -53,6 +53,33 @@ public class Solution {
         return max * max;
     }
 
+    //  https://leetcode.com/submissions/detail/62912583/
+    //
+    //  Submission Details
+    //  67 / 67 test cases passed.
+    //      Status: Accepted
+    //      Runtime: 164 ms
+    //          
+    //          Submitted: 0 minutes ago
+    public int MaximalSquare(char[,] matrix) {
+        var max = 0;
+        for (var i = 0; i < matrix.GetLength(0); i++) {
+            for (var j = 0; j < matrix.GetLength(1); j++) {
+                if (matrix[i, j] == '0') {
+                    continue;
+                }
+                
+                var up = i == 0 ? 0 : matrix[i - 1, j] - '0';
+                var left = j == 0 ? 0 : matrix[i, j - 1] - '0';
+                var upLeft = i == 0 || j == 0 ? 0 : matrix[i - 1, j - 1] - '0';
+                matrix[i, j] = (char) ('0' + 1 + Math.Min(upLeft, Math.Min(up, left)));
+                max = Math.Max(max, (int) Math.Pow(matrix[i, j] - '0', 2));
+            }
+        }
+        
+        return max;
+    }
+
     static void Main()
     {
         Console.WriteLine(new Solution().MaximalSquare(new char[,] {
