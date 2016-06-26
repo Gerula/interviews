97163ad4cd518da7bbf997c0004f15ee2409555e
Gerula <iacobandrei@gmail.com>
2016-06-25 19:07:21 -0700

    Serialize and deserialize binary tree - RDP

--- LeetCode/remote/serialize_and_deserialize_binary_tree.cs
+++ LeetCode/remote/serialize_and_deserialize_binary_tree.cs
@@ -50,6 +50,51 @@ public class Codec {
                      return acc;
                  }).Pop();
     }
+
+    // Recursive descent parser - TLE - probably from all the string replacements..
+    // (1(2)(3(4)(5)))
+    // Encodes a tree to a single string.
+    public string serialize(TreeNode root) {
+        if (root == null) {
+            return "()";
+        }
+        
+        return String.Format(
+            "({0}{1}{2})",
+            root.val,
+            serialize(root.left),
+            serialize(root.right));
+    }
+
+    // Decodes your encoded data to tree.
+    public TreeNode deserialize(string data) {
+        return deserialize(ref data);
+    }
+
+    public TreeNode deserialize(ref string data) {
+        if (String.IsNullOrWhiteSpace(data))
+        {
+            return null;
+        }
+        
+        if (data.StartsWith("()"))
+        {
+            data = data.Substring(2);
+            return null;
+        }
+        
+        var i = 1;
+        while (i++ < data.Length && data[i] != '(') {}
+        var val = int.Parse(data.Substring(1, i - 1));
+        data = data.Substring(i);
+        var result = new TreeNode(val) {
+            left = deserialize(ref data),
+            right = deserialize(ref data)
+        };
+        
+        data = data.Substring(1);
+        return result;
+    }
 }
 
 // Your Codec object will be instantiated and called as such:
