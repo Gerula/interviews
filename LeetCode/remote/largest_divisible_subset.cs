1441dbaff955a07683fa396727e35c691d12d5e8
Gerula <iacobandrei@gmail.com>
2016-06-27 06:26:24 -0700

    Largest divisible subset - good impl

--- LeetCode/remote/largest_divisible_subset.cs
+++ LeetCode/remote/largest_divisible_subset.cs
@@ -3,38 +3,50 @@
 //
 //   If there are multiple solutions, return any subset is fine. 
 
-//  Fails a test..
+//  https://leetcode.com/submissions/detail/65468755/
+//
+//  Submission Details
+//  29 / 29 test cases passed.
+//      Status: Accepted
+//      Runtime: 544 ms
+//          
+//          Submitted: 0 minutes ago
+//
 public class Solution {
     public int[] LargestDivisibleSubset(int[] nums) {
         if (nums.Length < 2) {
             return nums;
         }
         
-        var dp = new int[nums.Length];
+        Array.Sort(nums);
+        var dp = Enumerable.Repeat(1, nums.Length).ToArray();
         var prev = Enumerable.Repeat(-1, nums.Length).ToArray();
         var max = -1;
+        var maxIdx = -1;
         for (var i = 1; i < nums.Length; i++) {
             for (var j = 0; j < i; j++) {
-                if (nums[i] > nums[j] && 
-                    nums[i] % nums[j] == 0 &&
-                    dp[j] + 1 > dp[i])
+                if (nums[i] % nums[j] == 0 &&
+                    dp[j] + 1 >= dp[i])
                 {
                     dp[i] = dp[j] + 1;
                     prev[i] = j;
                 }
             }
             
-            if (max == -1 || dp[i] > dp[max]) {
-                max = i;
+            if (dp[i] > max) 
+            {
+                max = dp[i];
+                maxIdx = i;
             }
         }
-        
-        var result = new LinkedList<int>();
-        while (max != -1) {
-            result.AddFirst(nums[max]);
-            max = prev[max];
+    
+        Console.WriteLine(max);
+        var result = new int[max];
+        for (var i = max - 1; i >= 0; i--) {
+            result[i] = nums[maxIdx];
+            maxIdx = prev[maxIdx];
         }
         
-        return result.ToArray();
+        return result;
     }
 }
