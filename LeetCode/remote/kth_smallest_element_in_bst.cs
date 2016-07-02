160393cc8b454c6a968a092b11ef3ec16ef0cf22
Gerula <iacobandrei@gmail.com>
2016-07-02 13:32:47 -0700

    Kth smallest element in bst

--- LeetCode/remote/kth_smallest_element_in_bst.cs
+++ LeetCode/remote/kth_smallest_element_in_bst.cs
@@ -34,3 +34,44 @@ public class Solution {
         Smallest(root.right, ref index, ref node);
     }
 }
+
+//  Better than the previous as it can lead to the O(log n) solution. Granted it's slower but again,
+//  in repeating queries, we can hash the count values and then lead the the good solution.
+//  Fuck coding interviews.
+//
+//
+//  Submission Details
+//  91 / 91 test cases passed.
+//      Status: Accepted
+//      Runtime: 192 ms
+//          
+//          Submitted: 1 hour, 5 minutes ago
+//  https://leetcode.com/submissions/detail/65952474/
+
+public class Solution {
+    public int KthSmallest(TreeNode root, int k) {
+        if (root == null) {
+            return 0;
+        }
+        
+        var leftCount = CountNodes(root.left);
+        
+        if (leftCount + 1 == k) {
+            return root.val;
+        }
+        
+        if (leftCount >= k) {
+            return KthSmallest(root.left, k);
+        }
+        
+        return KthSmallest(root.right, k - leftCount - 1);
+    }
+    
+    private int CountNodes(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        
+        return 1 + CountNodes(root.left) + CountNodes(root.right);
+    }
+}
