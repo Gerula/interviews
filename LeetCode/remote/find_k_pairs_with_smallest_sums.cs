c9455f9cbf123b573162a4d3020b34c114acffc2
Gerula <iacobandrei@gmail.com>
2016-07-07 18:59:59 -0700

    Find k pairs with smallest sums - smarterter

--- LeetCode/remote/find_k_pairs_with_smallest_sums.cs
+++ LeetCode/remote/find_k_pairs_with_smallest_sums.cs
@@ -46,3 +46,67 @@ public class Solution {
     }
 }
 
+//  Intense struggle (and peeked at the solutions)
+//  https://leetcode.com/submissions/detail/66472584/
+//  
+//  Submission Details
+//  27 / 27 test cases passed.
+//      Status: Accepted
+//      Runtime: 668 ms
+//          
+//          Submitted: 0 minutes ago
+public class Solution {
+    public IList<int[]> KSmallestPairs(int[] nums1, int[] nums2, int k) {
+        /*
+            1   1   2
+        1   2   2   3
+        2   3   3   4
+        3   4   4   5
+        
+        [1,1],[1,1],[2,1],[1,2],[1,2],[2,2],[1,3],[1,3],[2,3]
+        
+            1   7   11
+        2   3   9   13
+        4   5   11  15
+        6   7   13  17
+        
+        [1,2],[1,4],[1,6],[7,2],[7,4],[11,2],[7,6],[11,4],[11,6]
+        
+        */
+        
+        var graph = new int[nums1.Length, nums2.Length];
+        for (var i = 0; i < nums1.Length; i++) {
+            for (var j = 0; j < nums2.Length; j++) {
+                graph[i, j] = nums1[i] + nums2[j];
+            }
+        }
+        
+        var result = new List<int[]>();
+        while (result.Count < k) {
+            var min = int.MaxValue;
+            var indexes = new [] { nums1.Length, nums2.Length };
+            for (var i = 0; i < nums1.Length; i++) {
+                // Note this shit right here. If it weren't for this, it would have TLEd
+                // this takes advantage on the fact that the matrix is already sorted
+                // and so it does pruning.
+                for (var j = 0; j < indexes[1]; j++) {
+                    if (graph[i, j] < min) {
+                        min = graph[i, j];
+                        indexes = new [] { i, j };
+                    } else {
+                        continue;
+                    }
+                }
+            }
+            
+            if (min == int.MaxValue) {
+                break;
+            }
+            
+            result.Add(new [] { nums1[indexes[0]], nums2[indexes[1]] });
+            graph[indexes[0], indexes[1]] = int.MaxValue;
+        }
+        
+        return result;
+    }
+}
